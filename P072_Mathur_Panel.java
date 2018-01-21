1af06edcde1dfeb37d56d6054e4574195091be37
garv mathur <garv.mathur@gmail.com>
2018-01-21 11:24:20 -0700

    Added 72 id panel

---
+++ P072_Mathur_Panel.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class P072_Mathur_Panel extends JPanel {
+    private JLabel fullName;
+    private JLabel clock;
+    private int counterValue;
+    private int num;
+    public final int WIDTH = 100;
+    public final int HEIGHT = 100;
+    public final int DELAY = 1000;
+
+    // Constructor
+    public P072_Mathur_Panel(int num){
+        this.num = num;
+        setPanelSize();
+        initializeandAndNameLabel();
+        setBackgroundColor();
+        setCounterValue();
+        setClockTimer();
+    }
+
+    // Sets the Counter Value depending on the parameter being odd or even
+    private void setCounterValue() {
+        if(isEven()){
+            counterValue = 0;
+        }
+        else{
+            counterValue = 9;
+        }
+    }
+
+    // Starts the clock timer. If the initial parameter is set as even the value increments. If odd it decrements.
+    private void setClockTimer() {
+        clock = new JLabel(String.valueOf(counterValue));
+        clock.setFont(new Font("Papyrus",Font.BOLD,16));
+        this.add(clock);
+
+        // Starts the timer
+        ActionListener taskPerformer = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(isEven() && counterValue == 9){
+                    counterValue = 0;
+                }
+                else if(!isEven() && counterValue == 0){
+                    counterValue = 9;
+                }
+                else if(isEven() == true){
+                    counterValue = counterValue + 1;
+                }
+                else{
+                    counterValue = counterValue -1;
+                }
+                clock.setText(String.valueOf(counterValue));
+            }
+        };
+        new Timer(DELAY, taskPerformer).start();
+    }
+
+    // Sets Background color of panel. If odd then sets light blue else sets White.
+    private void setBackgroundColor() {
+
+        if(isEven()){
+            this.setBackground(Color.WHITE);
+        }
+        else{
+            Color lightBlue = new Color(173,216,230);
+            this.setBackground(lightBlue);
+        }
+    }
+    // Initialize and Add  Name Label to panel
+    private void initializeandAndNameLabel() {
+        fullName = new JLabel("<html>Garv<br>Mathur</html>");
+        fullName.setFont(new Font("Papyrus",Font.BOLD,16));
+        this.add(fullName);
+    }
+
+    // Set the preferred panel size
+    private void setPanelSize() {
+        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+    }
+
+    // Checks if the initial configuration int value is even or odd
+    private boolean isEven(){
+        return num % 2 == 0;
+    }
+
+//    public static void main(String [] s)
+//    {
+//        int val = 3;
+//        new P072_Mathur_Panel(val);
+//    }
+}
