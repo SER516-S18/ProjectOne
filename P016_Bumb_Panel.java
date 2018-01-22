8c010a763ec92c54905ab640ede5d56742266e78
Chiragb7 <35378844+Chiragb7@users.noreply.github.com>
2018-01-22 14:38:31 -0700

    Update P016_Bumb_Panel.java

--- P016_Bumb_Panel.java
+++ P016_Bumb_Panel.java
@@ -41,7 +41,7 @@ public class P016_Bumb_Panel extends JPanel {
         if (type % 2 == 0)
             jp.setBackground(new Color(255, 255, 255));
         else
-            jp.setBackground(new Color(0, 255, 255));
+            jp.setBackground(new Color(171, 216,230));
         JLabel name = new JLabel("<html>Chirag<br> Bumb</html>", JLabel.CENTER);// JPanel with full name, first name on first row and last name on second row
         name.setFont(new Font("Papyrus", Font.PLAIN, 15);
         JLabel counter = new JLabel("",JLabel.CENTER);
