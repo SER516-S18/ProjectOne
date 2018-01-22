ac47a765750e6f2f738a0352bdd073f0edde8e77
rachanakashyap <rach.kashyap@gmail.com>
2018-01-21 23:31:35 -0700

    Add files via upload

---
+++ P053_Kashyap_Panel.java
@@ -0,0 +1,104 @@
+
+/*
+Author: Rachana Kashyap
+*/
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
+
+
+public class P053_Kashyap_Panel {
+    private JPanel panel;
+    private JLabel timer;
+    private boolean isEven;
+    
+    /* Function that creates a JPanel with JLables - name and timer */
+    public JPanel P053_Kashyap_Panel() {
+        panel = new JPanel();
+        panel.setSize(100, 100);
+        panel.setLayout(null);
+        JLabel name;
+        name = new JLabel("title",JLabel.CENTER);
+        name.setBounds(0,0,100,90);
+        
+        /*first name,last name in 2 rows*/
+        name.setText("<html><body><br><br>Rachana<br/>Kashyap<br/></body></html>");
+        name.setFont(new Font("Papyrus", Font.PLAIN, 12));
+        name.setLayout(null);
+        panel.add(name);
+        
+        /* check if isEven is true and set Background color appropriately */
+        if(isEven==false)
+                panel.setBackground(Color.decode("#add8e6"));/*Light blue color*/
+        else
+                panel.setBackground(Color.white);
+        /* create a JLabel for timer */
+        timer = new JLabel("0", JLabel.CENTER);
+        timer.setBounds(0,47,100,90);
+        timer.setFont(new Font("Papyrus", Font.PLAIN, 12));
+        timer.setLayout(null);
+        panel.add(timer);
+        
+        /*If even, increment counter from 0*/
+        if(isEven==true){
+            new Thread() {
+                int counter = 0;
+                public void run() {
+                    while(true) {
+                        if(counter==10)counter=0;	
+                        timer.setText(" " +counter++);
+                        try{
+                            Thread.sleep(1000);
+                        } 
+                        catch(Exception e) {}
+                    }
+                }
+            }.start();
+        } 
+        /* If odd, decrement counter from 9 */
+        else{
+            new Thread() {
+                int counter = 9;
+                public void run() {
+                    while(true) {
+                        if(counter==-1)counter=9;
+                        timer.setText(" " +counter--);
+                        try{
+                                Thread.sleep(1000);
+                        } 
+                        catch(Exception e) {}
+                    }
+                }
+            }.start();
+        }
+        return panel;
+    }
+	
+    /*Constructor*/
+    public P053_Kashyap_Panel(int val) {
+        /* if val is even, set boolean isEven to true */
+        if (val % 2 == 0){
+            isEven=true;
+            val = 0;
+        }
+        /* Else set it to false */
+        else{
+            isEven=false;
+            val = 9;
+        }
+    }
+    /* Main function */
+    public static void main(String[] args) {
+
+            P053_Kashyap_Panel myPanel = new P053_Kashyap_Panel(2);
+            JPanel panel = myPanel.P053_Kashyap_Panel();
+            JFrame myFrame = new JFrame("");
+            myFrame.setVisible(true);
+            myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myFrame.setSize(100, 150);
+            myFrame.add(panel);
+
+    }
+}
