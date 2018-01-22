3c300ec8cda60d9371b3c42f86eb75ed411e8886
raisa-khatun <raisakhatun@gmail.com>
2018-01-22 16:25:07 -0700

    Add files via upload
    Commented main ,frame and gave specified look

--- P056_Khatun_Panel.java
+++ P056_Khatun_Panel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,12 +10,13 @@ import javax.swing.JPanel;
 
 public class P056_Khatun_Panel {
 
-	
+	/*
 	public static void main(String args[])  
     {
 		//OBJECT OF THIS CLASS IS CREATED
-		new P056_Khatun_Panel(5);		
+		new P056_Khatun_Panel(4);		
     }
+    */
 	
 	//CONSTRUCTOR 
 	P056_Khatun_Panel(int x){
@@ -23,21 +25,25 @@ public class P056_Khatun_Panel {
 	
 	public void createPanel(int value)
 	{
+		//CREATING OBJ OF LIGHT BLUE COLOR 
+		Color color=new Color(173, 216, 230);
 		// CREATE PANEL + PANEL PROPERTIES
 		JPanel panel=new JPanel();
 		panel.setLayout(null);
 		panel.setBounds(20,20,200,200);
-		panel.setBackground(Color.decode("#3cbde7"));
-				
+		panel.setBackground(color);;
+		
 		// CREATE NAME LABEL + LABEL PROPERTIES
 		JLabel lblName = new JLabel("<html>Raisa<br>Khatun</html>");
 		lblName.setLayout(null);
 		lblName.setBounds(75,40,50,50);
+		lblName.setFont(new Font("Papyrus",Font.PLAIN, 15));
 		
 		// CREATE COUNTER LABEL + LABEL PROPERTIES
 		JLabel lblCounter = new JLabel();
 		lblCounter.setLayout(null);
 		lblCounter.setBounds(90,100,50,50);
+		lblCounter.setFont(new Font("Papyrus",Font.PLAIN, 15));
 		
 		// ADD LABELS TO PANEL
 		panel.add(lblName);
@@ -47,10 +53,10 @@ public class P056_Khatun_Panel {
 		JFrame frame= new JFrame("");
 
 		// ADD PANEL TO FRAME
-		frame.setSize(300, 300);
-		frame.setLayout(null);
-		frame.setVisible(true);
-		frame.add(panel);
+		//frame.setSize(300, 300);
+		//frame.setLayout(null);
+		//frame.setVisible(true);
+		//frame.add(panel);
 
 		Integer c = 0;
 		while(true)
@@ -82,7 +88,7 @@ public class P056_Khatun_Panel {
 
 			//IF VALUE IS NOT EVEN,BACKGROUND CYAN COUNTER DECREASES
 			else {
-					panel.setBackground(Color.decode("#3cbde7"));
+					panel.setBackground(color);
 					try {
 						for(c=9;c>0;c--) {								
 							lblCounter.setText(c.toString());								
