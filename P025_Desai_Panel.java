17cb513307040aed14666cfa3b9fe1727c12d5b1
dhrutidesai21 <35353350+dhrutidesai21@users.noreply.github.com>
2018-01-22 13:19:08 -0700

    Update P025_Desai_Panel.java

--- P025_Desai_Panel.java
+++ P025_Desai_Panel.java
@@ -17,7 +17,7 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class P025_Desai_Panel extends JPanel {
     private int timer;
     private int type;
-    private Font f = new Font("Papyrus", Font.BOLD, 16);
+    //private Font f = new Font("Papyrus", Font.BOLD, 15);
 
     /**
      * Class constructor.
@@ -45,7 +45,9 @@ public class P025_Desai_Panel extends JPanel {
         else
             jp.setBackground(new Color(0, 255, 255));
         JLabel name = new JLabel("<html>Dhruti<br> Desai</html>", JLabel.CENTER);
+        name.setFont(new Font("Papyrus", Font.PLAIN, 15));
         JLabel counter = new JLabel("",JLabel.CENTER);
+        counter.setFont(new Font("Papyrus", Font.PLAIN, 15));
 
         final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(new Runnable() {
