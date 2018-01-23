0afbeaeb66057c29ee96644949c255c150bfb661
AbdulSamad921 <ask428@live.com>
2018-01-23 12:28:08 -0700

    Add files via upload

--- P055_Khan_Panel.java
+++ P055_Khan_Panel.java
@@ -1,23 +1,18 @@
-/**
- *  For SER 516 only.
- * @author  Abdul Samad Khan	
- * @version 1.0
- * @since   01-21-2018
- */
-
 import java.awt.Color;
-
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 /**
  * 
+ * For SER 516 only.
+ * @author  Abdul Samad Khan	
+ * @version 1.0
+ * @since   01-21-2018
  * The class P055_Khan_Panel creates a JPanel including 2 JLabels
  * In this class there is a overloaded constructor that takes an integer value
  * depending on which if its even the color of the panel turns to white if its 
@@ -28,41 +23,38 @@ import javax.swing.Timer;
  */
 
 public class P055_Khan_Panel extends JPanel{
-	
-	private JLabel label1;
-	private JLabel label2;
+	private JLabel labelName;
+	private JLabel labelCounter;
 	private Timer timer;
+	public Color lightBlue = new Color(173,216,230);
 	/*
 	 * This function initializes the panel and the labels
 	 * and assign color to the panel according to the value 
 	 * given to the constructor.
 	 * 
-	 */
-	
+	 */	
 	public P055_Khan_Panel(int value){
 		try{
 			setSize(100,100);
 			setLayout(new FlowLayout());
 			setVisible(true);
-			label1=new JLabel("<html>Abdul Samad<br/>Khan</html>");
-			label1.setFont(new Font("Papyrus", Font.PLAIN, 15));
-			add(label1);
+			labelName=new JLabel("<html>Abdul Samad<br/>Khan</html>");
+			labelName.setFont(new Font("Papyrus", Font.PLAIN, 15));
+			add(labelName);
 			if(value%2==0)
-				{label2= new JLabel(String.valueOf( 0 ));
+				{labelCounter= new JLabel(String.valueOf( 0 ));
 				setBackground(Color.WHITE);
 				}
 			else
-				{label2= new JLabel(String.valueOf( 9 ));
-				setBackground(new Color(173,216,230));
+				{labelCounter= new JLabel(String.valueOf( 9 ));
+				setBackground(lightBlue);
 				}
-			label2.setFont(new Font("Papyrus", Font.PLAIN, 15));
-			add(label2);
+			labelCounter.setFont(new Font("Papyrus", Font.PLAIN, 15));
+			add(labelCounter);
 			timer(value);
 		}catch(Exception e){
 			e.printStackTrace();
-		}
-		
-		
+		}		
 	}
 	/*
 	 * This function handles how the counter increments or decrements
@@ -72,51 +64,28 @@ public class P055_Khan_Panel extends JPanel{
 	public void timer(int value){
 		try{
 		timer = new Timer(1000, new ActionListener(){
-
 			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
-				int counter = Integer.parseInt(label2.getText());
-				if(value % 2 ==0){
-			
+				int counter = Integer.parseInt(labelCounter.getText());
+				if(value % 2 ==0){			
 					counter++;
-					
 					if(counter>9){
 						counter=0;
 					}
 				}
 				else{
-					counter--;
-					
+					counter--;					
 					if(counter<0){
 						counter=9;
-					}
-					
+					}					
 				}
-				label2.setText(Integer.toString(counter));
-			
+				labelCounter.setText(Integer.toString(counter));
 			}
 					
 		});
 		timer.start();
 		}catch(Exception e){
 			e.printStackTrace();
-		}
-		
+		}		
 	}
-	/*
-	 * the main is used for testing 
-	 * public static void main(String []args){
-	 * P055_Khan_Panel p= new P055_Khan_Panel(1);
-	 * JFrame frame = new JFrame();
-	 * frame.setSize(100, 100);
-	 * frame.add(p);
-	 * frame.setVisible(true);
-	 * }
-	 */
-	
-	
-
-
-
-
 }
