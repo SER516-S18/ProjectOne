cac2c7a1a0ebae40d0fb3fdbc8ddb35de48d4cb6
tejasipalkar <31427342+tejasipalkar@users.noreply.github.com>
2018-01-23 00:00:34 -0700

    Update P083_Palkar_panel.java

--- P083_Palkar_panel.java
+++ P083_Palkar_panel.java
@@ -8,87 +8,86 @@ and  contains name of the author and a counter that runs from 0-9/9-0 added as l
 
 //package pkg516;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class P083_Palkar_panel extends JPanel{
 
 
-  public static int counter = 0;
-    private boolean EvenFlag;
-    private Color col;
+ public static int counter=0;
+	private JLabel Name;	//Private members
+	private JLabel Counter;	//Private members
+	private Color bgColor;	//Private members
+	
+	public P083_Palkar_Panel1(int ID) {
+		// TODO Auto-generated constructor stub
+		if(ID%2==0)
+		{
+			bgColor=Color.white;//Set panel color to white if ID is even
+			counter=-1;//Set counter value
+			CreatePanel(bgColor);//Create Panel with set background color
+			
+		}
+		else
+		{
+			bgColor=Color.BLUE;//Set panel color to white if ID is even
+			counter=10;//Set counter value
+			CreatePanel(bgColor);//Create Panel with set background color
+		}
+	}
 
-    public P083_Palkar_panel(int ID) {
-        if(ID%2==0)
-        {
-            
-            counter=-1;// Initialise counter value to -1
-            col=Color.white;//Set panel color to white
-            EvenFlag=true;//Set evenFlag to true if ID is even
-
-        }
-        else
-        {
-            
-            counter=10;// Initialise counter value to 10
-            col=Color.blue;//Set panel color to white
-           EvenFlag=false;//Set evenFlag to false if ID is even
-
-        }
-        setbgcol(col);//Function Call to set the background colour of the panel passing the set colour as parameter
-
-    }
-
-    public void setbgcol(Color c) 
-    {	
-       setSize(100,100);//Set the size of the panel
-        setLayout(new GridBagLayout());//For center alignment
-        GridBagConstraints a = new GridBagConstraints();//Create an object of GridBagConstraint
-        setBackground(c);//Set the background colour of the panel
-        JLabel Name = new JLabel("<html>Tejasi<br>Palkar</html>");//Create and initialise Name label with the name of the person.Represents the GUI component to be added on the panel
-        a.gridx=0;//Set the column address to O for the leftmost element
-        a.gridy=0;//Set the row address to 0
-        Name.setFont(new Font("Papyrus", Font.PLAIN, 15));
-        add(Name,a);//Add label onto panel
-
-        JLabel  Counter = new JLabel(String.valueOf(counter));//Create and Initialise Counter label with the counter value.Represents the gUI component to be added on the panel
-        Counter.setFont(new Font("Papyrus", Font.PLAIN, 15));
-       
-        a.gridx=0;//Set the column address for the counter to be aligned with the name label
-        a.gridy=1;//Set the row address as 1 for the counter to be diaplayed below the name label
-       add(Counter,a);//Add counter label onto the panel
-
-      
+	private void CreatePanel(Color bgColor) 
+	{
+		setSize(100,100);//Set panel size
+		setLayout(new GridBagLayout());
+		setBackground(bgColor);//Set background color with the passed value
+		
+		Name =new JLabel("<html>Tejasi<br>Palkar</html>");//Initialise name label
+		setFont(new Font("Papyrus", Font.PLAIN, 15));//Set font for the label
         
-        //Timer value change is kept between try and catch to handle exception at the runtime
-        try {
+        Counter = new JLabel(String.valueOf(counter));//Create and Initialise Counter label with the counter value.Represents the gUI component to be added on the panel
+        Counter.setFont(new Font("Papyrus", Font.PLAIN, 15));//Set the column address for the counter
+       
+        GridBagConstraints GbcObj= new GridBagConstraints();
+        GbcObj.gridx=0;//Set the column address for the name
+		GbcObj.gridy=0;//Set row address for name
+		add(Name,GbcObj);//Add label onto panel
+		
+		GbcObj.gridx=0;//Set the column address for the counter
+        GbcObj.gridy=1;//Set the row address as 1 for the counter below the name label
+        add(Counter,GbcObj);//Add counter label onto the panel
+        
+        RunTimer(bgColor);//Call function to run timer		
+	}
+
+	private void RunTimer(Color bgColor2)
+	{
+		// TODO Auto-generated method stub
+		try {
 
             TimerTask updateCounter = new TimerTask()//Create an Object of TimerTask Class
             {
                 public void run()//Method to change counter value
                 {
-                    if(EvenFlag)
+                    if(bgColor2==Color.WHITE)
                     {
                         
                         counter = (counter+1)%10;//Counter runs from 0 to 9 if ID is even
-                        
-
-
                     }
                     else
                     {
                         if(counter>0)
-                        {  
-                            counter = (counter-1)%10;//Counter runs from 9 to 0 if ID is odd
-                            
-                        }
-                        
+                        counter = (counter-1)%10;//Counter runs from 9 to 0 if ID is odd
                         else
-                            counter=9;
-
-
+                        counter=9;
                     }
 
                     Counter.setText(String.valueOf(counter));//Sets the value of Counter label with the current value and updates continously
@@ -108,17 +107,16 @@ public class P083_Palkar_panel extends JPanel{
 
 
     }
-
-
-
-  /*  
-   public static void main(String[] args) {
-        JFrame F = new JFrame("Frame_Palkar");
-        P083_Palkar_panel obj1 = new P083_Palkar_panel (81);
-        F.setVisible(true);
-        F.add(obj1);
-        F.pack();
-        F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }*/
+ 	
+ /*
+	public static void main(String[] args) 
+	{
+	        JFrame F = new JFrame("Frame_Palkar");
+	        P083_Palkar_Panel1 obj1 = new P083_Palkar_Panel1(83);
+	        F.setVisible(true);
+	        F.add(obj1);
+	        F.pack();
+	        F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    }*/
 
 }
