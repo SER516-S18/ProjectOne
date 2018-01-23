3012367fef21b2c134a13051baefc945f8bdf16f
Zelin Bao <35466865+baozelin@users.noreply.github.com>
2018-01-23 11:02:42 -0700

    fix the timer and size of labels

---
+++ P007_Bao_Panel.java
@@ -0,0 +1,90 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+
+
+
+
+public class P007_Bao_Panel extends JPanel implements Runnable{
+	public int height = 100;           //define height of label
+	public int wide = 100;             //define wide of label
+	int count = 0;						//timer
+	JPanel main = new JPanel(); 		//create a panel
+	JLabel name = new JLabel("<html><body>"+"Zelin"+"<br>"+"Bao"+"</html>",JLabel.CENTER);    //create a name label	
+	JLabel number = new JLabel("testing",JLabel.CENTER);										//create a timer label
+	private boolean input = false;		//decide increasing or decreasing
+	private Thread thread;  			//thread variable
+	
+	public P007_Bao_Panel(int num) {			
+		
+		//set font of labels
+	    number.setFont(new Font("Rockwell", 1, 15));	
+		name.setFont(new Font("Rockwell", 1, 15));		
+		
+		
+		//set panel
+		setSize(height,wide);
+		setLayout(new BorderLayout());
+		add(name, BorderLayout.NORTH);
+		add(number,BorderLayout.SOUTH);
+		setBackground(new Color(224, 255, 255));
+		
+		//decide input
+		if(num%2 == 0){
+			input = true;
+			setBackground(Color.WHITE);
+		}
+		
+		//run another thread
+		thread = new Thread(this);
+		thread.start();
+		
+	}
+	
+	//override run method 
+	public void run(){
+		try{
+			
+			if(input){		//increasing the timer
+				int i = 0;
+			
+				while(i <= 9){
+					String iValue = String.valueOf(i);
+					number.setText(iValue);
+					Thread.sleep(1000);
+					i++;
+					if(i > 9)	i = 0;
+					
+				 }
+		
+			}
+			else{		//decreasing the timer
+				int i = 9;
+				while(i >= 0){
+					String iValue = String.valueOf(i);
+					number.setText(iValue);
+					Thread.sleep(1000);
+					i--;
+					if(i < 0)	i = 9;
+				}
+			}
+			
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[]args){
+		//test 
+		JFrame jf = new JFrame();	//create a frame
+		P007_Bao_Panel p = new P007_Bao_Panel(1);	
+		//new Thread(p).start();	
+		jf.setSize(800,800);
+		jf.setVisible(true);
+		jf.add(p);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+}
