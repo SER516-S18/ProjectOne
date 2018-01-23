129e7d543272839c2a1fb5bc0f46e2a39a77347a
raisa-khatun <raisakhatun@gmail.com>
2018-01-23 04:55:00 -0700

    Add files via upload

--- P056_Khatun_Panel.java
+++ P056_Khatun_Panel.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
-
-import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -9,103 +9,116 @@ import javax.swing.JPanel;
 
 public class P056_Khatun_Panel extends JPanel {
 	
-	private static final long serialVersionUID = 1L;
-	Color color;
-	Boolean isEven;
 	JLabel lblCounter;
+	JLabel lblName;
+	Boolean isEven;
 	
 	//CONSTRUCTOR 
 	public P056_Khatun_Panel(int value){
+	
+		// PANEL PROPERTIES
+		setLayout(new FlowLayout());
+		setVisible(true);
 		
-		Color blue = new Color(173, 216, 230);
-		Color white = new Color(255, 255, 255);
-		
-		if(value%2==0)
-		{
-			color = white;
-			isEven = true;
-			
-		}
-
-		//IF VALUE IS NOT EVEN,BACKGROUND LIGHT BLUE COUNTER DECREASES
-		else {
-				color = blue;
-				isEven = false;
-			
-		}
-
-		// CREATE PANEL + PANEL PROPERTIES
-		//JPanel panel=new JPanel();
-		this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
-		this.setBounds(20,20,200,200);
-		this.setBackground(color);;
-		
-		// CREATE NAME LABEL + LABEL PROPERTIES
+		// NAME LABEL PROPERTIES
 		JLabel lblName = new JLabel("<html>Raisa<br>Khatun</html>");
-		lblName.setLayout(null);
 		lblName.setBounds(75,40,50,50);
 		lblName.setFont(new Font("Papyrus",Font.PLAIN, 15));
 		
-		// CREATE COUNTER LABEL + LABEL PROPERTIES
-		lblCounter.setLayout(null);
+		// COUNTER LABEL PROPERTIES
+		lblCounter = new JLabel();
 		lblCounter.setBounds(90,100,50,50);
 		lblCounter.setFont(new Font("Papyrus",Font.PLAIN, 15));
 		
 		// ADD LABELS TO PANEL
-		this.add(lblName);
-		this.add(lblCounter);	
+		add(lblName);
+		add(lblCounter);
 		
-		Integer c = 0;
-		while(true)
+		// BACKGROUND COLOR SET
+		if(value%2==0)
 		{
-			//IF VALUE IS EVEN, COUNTER INCREASES
-			if(isEven)
-			{
-				try {					
-					for(c=0;c<9;c++)
-					{
-						lblCounter.setText(c.toString());					
-						Thread.sleep(1000);
-					}										
-				} catch (InterruptedException e) {
-						e.printStackTrace();
-				}
+			setBackground(Color.decode("#ffffff"));
+			isEven=true;		
+		}
+		else
+		{
+			setBackground(new Color(173,216,230));
+			isEven=false;
+		}
 		
+		CounterDetails();
+			
+	}
+	
+	public void CounterDetails()
+	{
+		Thread thread = new Thread(new Runnable() {
+
+			Integer c = 0;
+			
+			@Override
+			public void run() {
+				
 				try {
-					for(c=9;c>0;c--)
-					{						
-						lblCounter.setText(c.toString());							
-						Thread.sleep(1000);
-					}
-				} catch (InterruptedException e) {
+					while(true)
+					{
+						//EVEN
+						if(isEven)
+						{			
+							// 0 TO 9
+							for(c=0;c<9;c++)
+							{
+								lblCounter.setText(c.toString());					
+								Thread.sleep(1000);
+							}										
+
+							// 9 TO 0
+							for(c=9;c>0;c--)
+							{						
+								lblCounter.setText(c.toString());
+								Thread.sleep(1000);
+							}
+						}
+						
+						//ODD
+						else {
+							
+							// 9 TO 0
+							for(c=9;c>0;c--)
+							{								
+								lblCounter.setText(c.toString());								
+								Thread.sleep(1000);
+							}
+							
+							// 0 TO 9
+							for(c=0;c<9;c++)
+							{
+								lblCounter.setText(c.toString());						
+								Thread.sleep(1000);
+							}
+						}
+					
+					} // END WHILE
+					
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			}
-
-			//IF VALUE IS NOT EVEN, COUNTER DECREASES
-			else {
-					try {
-						for(c=9;c>0;c--) {								
-							lblCounter.setText(c.toString());								
-							Thread.sleep(1000);
-						}
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
 				
-					try {						
-						for(c=0;c<9;c++)
-						{
-							lblCounter.setText(c.toString());						
-							Thread.sleep(1000);
-						}						
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-			}
-			
-		}//END OF WHILE LOOP				
+			}// END RUN
+		});	
+		
+		thread.start();
+	}
 
-	}// END OF CONSTRUCTOR
-
+	/* TO TEST
+	public static void main(String args[])     {
+		 
+		P056_Khatun_Panel p= new P056_Khatun_Panel(3);
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.add(p);
+		frame.setVisible(true);
+	}
+	*/
+	
 }//END OF CLASS
