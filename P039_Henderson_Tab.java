6545966c8ba47936e1b0ae7db3f64f2b639945bd
christopher-henderson <chris@chenderson.org>
2018-01-23 15:39:56 -0700

    a nicer way of incrementing. Plus nuking a DS_Store that someone commited.

--- P039_Henderson_Tab.java
+++ P039_Henderson_Tab.java
@@ -13,38 +13,43 @@ import java.awt.Color;
 public class P039_Henderson_Tab extends JPanel {
 
 	public static final String TABNAME = "Chris Henderson";
+	public static final Color LIGHTBLUE = new Color(173,216,230);
 
 	public P039_Henderson_Tab() {
+		// Panels expect an integer in their constructor which determines
+		// whether they will be an incrementor with a white background (even)
+		// or a decrementor with a light blue background (odd).
+		int typeSwitch = 0;
 		this.setLayout(new GridLayout(5, 5));
-		this.add(new P027_Dua_Panel(1));
-		this.add(new P028_Dusad_Panel(2));
-		this.add(new P029_Dutta_Panel(3));
+		this.add(new P027_Dua_Panel(typeSwitch++));
+		this.add(new P028_Dusad_Panel(typeSwitch++));
+		this.add(new P029_Dutta_Panel(typeSwitch++));
 		// 30 is the Main.
-		this.add(new P031_Faldu_Panel(5));
-		this.add(new P032_Gadde_Panel(6));
-		this.add(new P033_Goel_Panel(7));
-		this.add(new P034_Gorantla_Panel(8));
-		this.add(new P035_Gupta_Panel(9));
-		this.add(new P036_Gupta_Panel(10));
-		this.add(new P037_Gupta_Panel(11));
+		this.add(new P031_Faldu_Panel(typeSwitch++));
+		this.add(new P032_Gadde_Panel(typeSwitch++));
+		this.add(new P033_Goel_Panel(typeSwitch++));
+		this.add(new P034_Gorantla_Panel(typeSwitch++));
+		this.add(new P035_Gupta_Panel(typeSwitch++));
+		this.add(new P036_Gupta_Panel(typeSwitch++));
+		this.add(new P037_Gupta_Panel(typeSwitch++));
 		// 39 is the tab author.
-		this.add(new P038_Gupta_Panel(12));
-		this.add(new P040_Henderson_Panel(13));
-		this.add(new P041_Hosahalli_Panel(14));
+		this.add(new P038_Gupta_Panel(typeSwitch++));
+		this.add(new P040_Henderson_Panel(typeSwitch++));
+		this.add(new P041_Hosahalli_Panel(typeSwitch++));
 		// Is not a panel. Will not work. Does not appear to be done.
 		// this.add(new P042_Yiru_Panel(15));
-		this.add(new P043_Iyer_Panel(16));
-		this.add(new P044_Jaime_Panel(17));
-		this.add(new P045_JOHAR_Panel(18));
+		this.add(new P043_Iyer_Panel(typeSwitch++));
+		this.add(new P044_Jaime_Panel(typeSwitch++));
+		this.add(new P045_JOHAR_Panel(typeSwitch++));
 		// this.add(new P046_NAME_Panel(19)); Does not exist.
-		this.add(new P047_Kakarla_Panel(20));
-		this.add(new P048_Kalluri_Panel(21));
-		this.add(new P049_Kandimalla_Panel(22));
-		this.add(new P050_Kapadia_Panel(23));
-		this.add(new P051_Kapoor_Panel(24));
-		this.add(new P052_Kasam_Panel(25));
+		this.add(new P047_Kakarla_Panel(typeSwitch++));
+		this.add(new P048_Kalluri_Panel(typeSwitch++));
+		this.add(new P049_Kandimalla_Panel(typeSwitch++));
+		this.add(new P050_Kapadia_Panel(typeSwitch++));
+		this.add(new P051_Kapoor_Panel(typeSwitch++));
+		this.add(new P052_Kasam_Panel(typeSwitch++));
 		// This person swears that they are group 2. Could be
 		// because the main author is in our block.
-		this.add(new P131_Zhou_Panel(25));
+		this.add(new P131_Zhou_Panel(typeSwitch++));
 	}
 }
