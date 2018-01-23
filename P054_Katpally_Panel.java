f3d7c00aa70fd91e1d287c8a7d86dabf04989851
Harshitha Katpally <32940031+harshithakatpally@users.noreply.github.com>
2018-01-23 13:30:52 -0700

    Updated the constructor.

--- P054_Katpally_Panel.java
+++ P054_Katpally_Panel.java
@@ -2,116 +2,77 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
-public class P054_Katpally_Panel{
+public class P054_Katpally_Panel
+{
+	private JPanel panel;
+	private JLabel firstname;
+	private JLabel lastname;
+	private JLabel counter;
+	private Timer timer;
+	private int i;
+	private static final Color LIGHTBLUE = new Color(173, 216, 230);
 	
-	JPanel panel;
-	JLabel label1;
-	JLabel label2;
-	JLabel label3;
-	Color color;
-	Timer timer;
-	int flag = 1;
-	int c;
-	
-	/*Constructor of the Panel class that takes an integer input based on which the 
-	 * background color of the panel is decided
+	/*Constructor of the class that takes an integer input based on which the 
+	 * background color of the panel is decided.
 	 */
-	public P054_Katpally_Panel(int x) 
+	public P054_Katpally_Panel(int counterdecider) 
 	{
-		if(x%2 == 0)
-		{
-			color = Color.WHITE;
-			flag = 0;
-		}
-		else
-		{	
-			color = new Color(173, 216, 230);
-		}
-	}
-	
-	/*Function to create a JPanel with three JLabels and return the panel*/
-	public JPanel createPanel()  
-	{
-		
-		
-	    label1 = new JLabel("Harshitha");
-	    label1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-	    label1.setFont(new Font("Papyrus", Font.PLAIN, 15));
+		firstname = new JLabel("Harshitha");
+	    	firstname.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+	    	firstname.setFont(new Font("Papyrus", Font.PLAIN, 15));
 	    
-	    label2 = new JLabel("Katpally");
-	    label2.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-	    label2.setFont(new Font("Papyrus", Font.PLAIN, 15));
+	    	lastname = new JLabel("Katpally");
+	    	lastname.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+	    	lastname.setFont(new Font("Papyrus", Font.PLAIN, 15));
 	    
-	    label3 = new JLabel("Text");
-	    label3.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-	    label3.setFont(new Font("Papyrus", Font.PLAIN, 15));
+	    	counter = new JLabel("Text");
+	    	counter.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+	    	counter.setFont(new Font("Papyrus", Font.PLAIN, 15));
 	  
-	    panel = new JPanel();
-	    panel.setSize(100, 100);
-	    panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+	    	panel = new JPanel();
+	    	panel.setSize(100, 100);
+	    	panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-	    /*Using Timer class to display the counter depending on the argument passed while object creation*/
-	    	if(flag == 0)
+	    	/*Using Timer class to display the counter and setting background color 
+	    	* depending on the argument passed while object creation.
+	     	*/
+	    	if(counterdecider%2 == 0)
 	    	{
-	    		c = 0;
+	    		i = 0;
+	    		panel.setBackground(Color.WHITE);
 	    		timer = new Timer(1000, new ActionListener() {
 	    			@Override
 	    			public void actionPerformed(ActionEvent e) {
-	    				if(c > 9)
+	    				if(i > 9)
 	    				{
-	    					c = 0;
+	    					i = 0;
 	    				}
-	    				label3.setText(Integer.toString(c));
-	    				c++;	  				
+	    				counter.setText(Integer.toString(i));
+	    				i++;
 	    			}
 	    		});
 	    	}
 	    	else
 	    	{
-	    		c = 9;
+	    		i = 9;
+	    		panel.setBackground(LIGHTBLUE);
 	    		timer = new Timer(1000, new ActionListener() {
 	    			@Override
 	    			public void actionPerformed(ActionEvent e) {
-	    				if(c < 0)
-	    				{
-	    					c = 9;
-	    				}
-	    				label3.setText(Integer.toString(c));
-	    				c--;	    				
-	    			}
+	  				if(i < 0)
+	  	 			{
+	  	  				i = 9;
+	  	  			}
+	  	  			counter.setText(Integer.toString(i));
+	  	  			i--;			
+	  	  		}
 	    		});
 	    	}
-	    		
-	    
-	    timer.start();
-	    panel.add(label1);
-	    panel.add(label2);
-	    panel.add(label3);
-	    panel.setBackground(color);
-
-	    return panel;
-	}	
-
-	/*Main Function to test the creation of a JPanel*/
-	/*public static void main(String[] args) 
-	{
-		
-		int a = 6;
-		P054_Katpally_Panel pobj = new P054_Katpally_Panel(a);
-		
-		JFrame frame = new JFrame("Test");
-	    frame.setVisible(true);
-	    frame.setLayout(null);
-	    frame.setSize(500, 500);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
-	    JPanel p = pobj.createPanel();
-	    frame.add(p);
-		
-		
-	}*/
-
+	    	timer.start();
+	    	panel.add(firstname);
+	    	panel.add(lastname);
+		panel.add(counter); 
+	}
 }
