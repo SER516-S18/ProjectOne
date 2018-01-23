092ae51e924e31212fe74ea539f30546b4bb9adb
aashita-priya <31880562+aashita-priya@users.noreply.github.com>
2018-01-23 08:11:37 -0700

    Update P092_Priya_Panel.java

--- P092_Priya_Panel.java
+++ P092_Priya_Panel.java
@@ -6,43 +6,80 @@ import java.util.TimerTask;
 import java.util.Timer;
 import java.util.logging.Logger;
 
+/**
+ * P092_Priya_Panel.java
+ * @author  Aashita Priya
+ */
+
 public class P092_Priya_Panel {
 
     private int colorFlag;
 
     private static final Logger LOGGER = Logger.getLogger(P092_Priya_Panel.class.getSimpleName());
 
+    /**
+     * Constructor with a parameter for color flag
+     * @param colorFlag
+     */
     public P092_Priya_Panel(int colorFlag) {
         this.colorFlag = colorFlag;
     }
 
+    /**
+     *
+     * @return JPanel Object to use
+     */
     public JPanel getJPanel() {
 
+        //Timer for counter
         Timer timer = new Timer();
 
         final int[] timeElapsedSec = {0};
 
-        JPanel panel = new JPanel();
+        //Creating a JPanel object
+        JPanel panel = new JPanel(new GridBagLayout());
         try {
 
-            Font font = new Font("Papyrus", Font.PLAIN, 12);
+            //Creating a Font object with given constraints
+            Font font = new Font("Papyrus", Font.PLAIN, 15);
 
+            //Creating labels with FirstName LastName and tick
             JLabel firstName = new JLabel("Aashita");
             JLabel lastName = new JLabel("Priya");
+            JLabel tick = new JLabel();
 
-            firstName.setAlignmentX(Component.CENTER_ALIGNMENT);
-            lastName.setAlignmentX(Component.CENTER_ALIGNMENT);
+            //Creating constraints for the positions of each label
+            GridBagConstraints fNameConstraints = new GridBagConstraints();
+            GridBagConstraints lNameConstraints = new GridBagConstraints();
+            GridBagConstraints timerConstraints = new GridBagConstraints();
 
+            //Setting preferred size of the panel according to the given specifications
+            panel.setPreferredSize(new Dimension(100, 100));
+
+            //Setting constraints for FirstName Label
+            fNameConstraints.gridx = 0;
+            fNameConstraints.gridy = 0;
+
+            //Setting constraints for LastName Label
+            lNameConstraints.gridx = 0;
+            lNameConstraints.gridy = 2;
+
+            //Setting constraints for Tick Label
+            timerConstraints.gridx = 0;
+            timerConstraints.gridy = 4;
+
+            //Set Fonts for all the labels
             firstName.setFont(font);
             lastName.setFont(font);
-
-            JLabel tick = new JLabel();
             tick.setFont(font);
-            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-            panel.add(firstName, Component.CENTER_ALIGNMENT);
-            panel.add(lastName, Component.CENTER_ALIGNMENT);
-            if (colorFlag % 2 == 0) {
 
+            //Add FirstName and LastName to the panel
+            panel.add(firstName, fNameConstraints);
+            panel.add(lastName, lNameConstraints);
+
+            //The logic for Ticker
+            //If the constructor is passing even then count up from 0 else count down from 9
+            if (colorFlag % 2 == 0) {
                 timeElapsedSec[0] = 0;
                 panel.setBackground(Color.white);
                 timer.schedule(new TimerTask() {
@@ -72,25 +109,21 @@ public class P092_Priya_Panel {
                 }, 1000, 1000);
 
             }
-            panel.add(tick);
+            //Add Tick to the panel
+            panel.add(tick, timerConstraints);
 
         } catch (Exception e) {
             LOGGER.severe("Exception Occurred:" + e.getMessage());
         }
-
         return panel;
     }
 
-    public static void main(String[] args) {
-        // TODO: I have commented out the main function, which will be written by the second developer. I have written some code which should be a good starting point for the developer
-
-        JFrame frame;
-        frame = new JFrame("P092_Priya_Panel");
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(100,100));
-        P092_Priya_Panel obj = new P092_Priya_Panel(91);
-        frame.getContentPane().add(obj.getJPanel());
-    }
+    /**
+     * How to use
+     * 1. Create an object of P092_Priya_Panel [ P092_Priya_Panel panelCreate = new P092_Priya_Panel(1); ]
+     * 2. Create Frame
+     * 3. Add panel to Frame using [ add(panelCreate.getPanel()); ]
+     * 4. Pack the Frame [pack()]
+     * 5. Set Visible to true [ setLocationRelativeTo(null); ]
+     */
 }
