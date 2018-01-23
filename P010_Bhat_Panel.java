5ff764ca3fce4de8098e25ea46c10a46c01d1980
mohan1967 <35353725+mohan1967@users.noreply.github.com>
2018-01-22 20:05:45 -0700

    Update P010_Bhat_Panel.java

--- P010_Bhat_Panel.java
+++ P010_Bhat_Panel.java
@@ -1,119 +1,113 @@
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
- * SER: 516(SOFTWARE AGILITY)
- * P010_Bhat_Panel implements assignment one. A panel has two labels-
- * one that displays my full name, and the other that displays a counter.
  * 
- * @author kritikabhat, ASU ID: kbhat3
- * @since 2018-01-20
- * @param fullName JLabel that contains First and Second Name
- * @param displayNumber JLabel that contains the timer value
- * @param timer an integer to control counter increase/decrease
- * @param controlInteger the parameter passed to constructor
+ * @author Anusha Chittoor Prem
+ * Contains two Jlabels for name and a number used as counter.
+ * If the value passed is even, then the background is set to white and the counter increments.
+ * If the value passed is odd, then the background is set to blue and the counter decrements. 
  */
 
-public class P010_Bhat_Panel extends JPanel {
-		private JLabel fullName;
-		private JLabel displayNumber;
+public class P022_ChittoorPrem_Panel extends JPanel {
+	private JLabel name, number;
+	private int counter;
+	
+	// Constructor with an integer as a parameter.
+	public P022_ChittoorPrem_Panel(int num) {
+		Font font = new Font("Papyrus", Font.PLAIN, 12);
+		setLayout(new GridLayout(2,1));
+		//instantiate name and number
+		name = new JLabel("<html><center>Anusha<br>Chittoor Prem</center></html>",JLabel.CENTER);
+		number = new JLabel();
+		//set positions and font of name and number
+		name.setHorizontalAlignment(JLabel.CENTER);
+		number.setHorizontalAlignment(JLabel.CENTER);
+		name.setForeground(Color.black);
+		number.setForeground(Color.black);
+		name.setFont(font);
+		number.setFont(font);
 		
-		public P010_Bhat_Panel(int controlInteger){
-			
-			/**
-			 * This constructor creates the JPanel, changes the background and sets timer depending
-			 * on the value of passed integer- whether it is even or odd.
-			 */
-		 	
-	 		this.setPreferredSize(new Dimension(100,100));
-	 		this.setLayout(new GridLayout(2,1));
-	 		displayNumber = new JLabel();
-	 		fullName = new JLabel("<html>Kritika<br> Bhat</html>");
-	 		fullName.setFont(new Font("Papyrus", Font.PLAIN, 15));
-	 		this.add(fullName);
-	 		this.add(displayNumber);
-	 		fullName.setHorizontalAlignment(JLabel.CENTER);
-	 		displayNumber.setHorizontalAlignment(JLabel.CENTER);
-	 		this.setVisible(true);	 		
-	 		
-	 		/**
-	 		 *The background color and counter value are decided here 
-	 		 */
-	 		
-	 		if(controlInteger % 2 == 0) { 
-	 			this.setBackground(Color.white);
-	 			method_timer(controlInteger);	 			
-	 		}
-	 		else { 
-	 			this.setBackground(new Color(173,216,230));
-	 			method_timer(controlInteger);			 	    
-	 		}
+		//add them to the panel
+		add(name);
+		add(number);
+		//if value passed is even
+		if(num%2 == 0) {
+			this.setBackground(new Color(255,255,255));
+			increment();
+		}else{ //if value passed is odd
+			this.setBackground(new Color(173,216,230));
+			decrement();
 		}
+		//set size of the panel
+		this.setPreferredSize(new Dimension(100,100));
+		this.setVisible(true);
+	}
+	
+	
+	//method to increment the counter by one step
+	public void increment() {
+		counter = 0;
+		new Thread() {
+			public void run() {
+				while(true) {
+					number.setText(""+counter);
+					//sets back the counter to 0 when the counter reaches 10
+					if(++counter == 10) {
+						counter = 0;	
+					}
+					try {
+						//sleeps for 1second. 
+						Thread.sleep(1000);
+					}catch(Exception e) {
+						System.out.println(e);
+					}
+				}
+			}
+		}.start();
+	}
+	
+	
+	//method to decrement the counter by one step
+	public void decrement() {
+		counter = 9;
+		new Thread() {
+			public void run() {
+				while(true) {
+					number.setText(""+counter);
+					//sets back the counter value to 9 when the counter reaches negative value
+					if(--counter < 0) {
+						counter = 9;	
+					}
+					try {
+						Thread.sleep(1000);
+					}catch(Exception e) {
+						System.out.println(e);
+					}
+				}
+			}
+		}.start();
+	}
+	
+	
+	//main function for testing
+	public static void main(String []args) {		
+		P022_ChittoorPrem_Panel p = new P022_ChittoorPrem_Panel(22);
+		JFrame jframe = new JFrame("Java Applaiction");
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		mainPanel.add(p);
+		jframe.setSize(100,100);
+		jframe.setContentPane(mainPanel);
+		jframe.setVisible(true);
+	}
 
-		private void method_timer(int n){
-			
-			/**
-			 * if the passed integer is even, the timer increments from 0 to 9
-			 */
-			
-			if(n % 2 == 0) { 
-				new Thread() {
-			        int timer = 0;
-			        public void run() {
-			            while(true) {
-			            		if(timer == 10) timer = 0;
-			            	displayNumber.setText("" + (timer));
-			                timer++;
-			                try{
-			                    Thread.sleep(1000);
-			                } catch(Exception e) {}
-			            }
-			        }
-			    }.start();
-			}
-			
-			/**
-			 * If the passed integer is odd, the timer decrements from 9 to 0
-			 */
-			
-			else { 
-				new Thread() {
-			        int timer = 9;
-			        public void run() {
-			            while(true) {
-			            		if(timer == -1) timer = 9;
-			                displayNumber.setText("" + (timer));
-			                timer--;
-			                try{
-			                    Thread.sleep(1000);
-			                } catch(Exception e) {}
-			            }
-			        }
-			    }.start();
-			}
-		}
-		
-		
-		/**
-		 * The main method was used for testing only.
-		 * 
-		 * @param args command line arguments
-		 *  
-		public static void main(String[] args) {
-			
-			//creating the panel
-			P010_Bhat_Panel p = new P010_Bhat_Panel(6);
-			JFrame f = new JFrame();
-			
-			//just style for JFrame for testing
-			f.getContentPane().add(p);
-			f.pack();
-			f.setVisible(true);
-			f.setSize(800,800);
-			f.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 90));
-			
-			//testing to make sure threads are working properly
-			System.out.print("Testing");
-		}
-		*/
 }
