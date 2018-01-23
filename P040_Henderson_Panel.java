022913ecb73f4d87521ede17fceb99df99c01142
David Henderson <therealdavidhenderson@gmail.com>
2018-01-23 12:14:26 -0700

    Updated P040 panel with Papyrus

--- P040_Henderson_Panel.java
+++ P040_Henderson_Panel.java
@@ -1,24 +1,27 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
-import javax.swing.WindowConstants;
 
 /**
  * P040_Henderson_Panel, a panel for ProjectOne, featuring a countdown timer and the author's name
  *
- * @author  David Henderson
+ * @author  David Henderson (dchende2@asu.edu)
  * @version 1.0
  * @since   2018-01-18
  */
 public class P040_Henderson_Panel extends JPanel {
-	protected JLabel labelName = new JLabel("<html><center>David<br>Henderson</center></html>");
-	protected JCountdownLabel labelCounter;
+	// Public constants, available to change if necessary
+	public static Color LIGHT_BLUE = new Color(189,214,238);
+	public static Font FONT = new Font("Papyrus", Font.PLAIN, 15);
+	
+	protected final JLabel labelName = new JLabel("<html><center>David<br>Henderson</center></html>");
+	protected final JCountdownLabel labelCounter;
 
 	/**
 	 * Constructor to create the panel.
@@ -27,6 +30,7 @@ public class P040_Henderson_Panel extends JPanel {
 	public P040_Henderson_Panel(int value) {
 		this.setLayout(new BorderLayout()); // Set the panel layout
 		
+		labelName.setFont(FONT);
 		labelName.setHorizontalAlignment(SwingConstants.CENTER);
 		labelName.setAlignmentX(0.5f);
 		this.add(labelName, BorderLayout.PAGE_START); // Add the name label to the panel, at the top
@@ -37,10 +41,11 @@ public class P040_Henderson_Panel extends JPanel {
 			labelCounter = new JCountdownLabel(true);
 		} else {
 			// Otherwise per spec, if value is odd set the panel background to light blue and count down
-			this.setBackground(new Color(189,214,238));
+			this.setBackground(LIGHT_BLUE);
 			labelCounter = new JCountdownLabel(false);
 		}
 
+		labelCounter.setFont(FONT);
 		labelCounter.setHorizontalAlignment(SwingConstants.CENTER);
 		labelCounter.setAlignmentX(0.5f);
 		this.add(labelCounter, BorderLayout.CENTER); // Add the countdown label
@@ -71,24 +76,9 @@ public class P040_Henderson_Panel extends JPanel {
 			};
 
 			// Timer to increment the value and update the label
-			try {
-				Timer timer = new Timer(1000, task);
-				timer.setRepeats(true);
-				timer.start();
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
+			Timer timer = new Timer(1000, task);
+			timer.setRepeats(true);
+			timer.start();
 		}
 	}
-	
-	// For testing, left in per specification
-	// public static void main(String[] args) {
-	//     JFrame frame = new JFrame("SER516 ProjectOne - David Henderson");
-	//     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	//     frame.setLayout(new BorderLayout());
-	//     frame.add(new P040_Henderson_Panel(0), BorderLayout.PAGE_START);
-	//     frame.add(new P040_Henderson_Panel(1), BorderLayout.PAGE_END);
-	//     frame.pack();
-	//     frame.setVisible(true);
-	// }
 }
