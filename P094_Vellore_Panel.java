51d59d4a713d08390c612af08e407dda6c8a573a
VaishakVellore <vaishak.vellore@gmail.com>
2018-01-23 13:58:58 -0700

    Add files via upload

--- P094_Vellore_Panel.java
+++ P094_Vellore_Panel.java
@@ -1,81 +1,94 @@
-/*
-Lab1
-Author: Vaishak Ramesh Vellore
-The class creates a Panel with First and Last Name's of a person.
-A counter is incremented and decremented from 0-9 or 9-0.
-The background color changes depending on the integer passed to the constructor.
-*/
-
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * @author Vaishak Ramesh Vellore
+ * @date 01/21/2018
+ * @description A Panel class for SER516 project.
+ *
+ * Class Extends JPanel and contains author's name and a Counter. It contains one parameterized constructor
+ * that accepts an integer value.
+ * Panel background is set to WHITE (255,255,255) if value is even or LIGTH BLUE (173,216,230) if odd.
+ * Counter increments from 0-9 if value is even or decrements from 9-0 if odd.
+*/ 
 
-public class P094_Vellore_Panel {
-	/* The class consists of a JPanel with the Firstname and Lastname*/
-	//The JPanel is of size 100*100 
-	private JPanel panel;
-	private JLabel name;
-	private JLabel timer;
+public class P094_Vellore_Panel extends JPanel{ 
+	
+	//Initializing panel and count label
+	private JLabel name = new JLabel("<html>Vaishak<br>Ramesh Vellore</html>");
+	private JLabel timer= new JLabel();
 	private boolean var;
 	
-	public JPanel P094_Vellore_Panel() {
-		
-		//setting up JPanel.
-		panel=new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		
-		if(var==false)
-			panel.setBackground(Color.blue);
-		else
-			panel.setBackground(Color.white);
-		
-		//setting the size of the panel x,y axis
-		panel.setPreferredSize(new Dimension(100,100));
-		panel.setMaximumSize( panel.getPreferredSize() );
-		
-		//setting up JLabel
-		//lable with first and last name details
-		name = new JLabel("<html><br>Vaishak<br>Ramesh Vellore</html>");
-		timer=new JLabel();
-		
-		//Resetting the font for both the variables
-		Font font = new Font("Papyrus", Font.PLAIN, 15);
-		name.setFont(font);
-		timer.setFont(font);
-				
-		name.setHorizontalAlignment(JLabel.CENTER);
-	    timer.setHorizontalAlignment(JLabel.CENTER);
-		
-		panel.add(name);
-		panel.add(timer);
-		panel.setVisible(true);
-		
-		//setting up counter. 
-		if(var==true) 
-			TimerIncrement();
-		else
-			TimerDecrement();
-		
-		return panel;
-	}//end of JLabel
+	
+	public enum NewColors{
+        LIGHTBLUE(new Color(173,216,230)), WHITE(Color.WHITE);
+        private Color color;
+        private NewColors(Color color)
+        {
+            this.color=color;
+        }
+        public Color setColor() {
+            return color;
+        }
+    }
 	
 	
 	//Constructor for the class
-		public P094_Vellore_Panel(int num) {
-			/* A parameter is sent to the constructor and if this number is even then it is set to true*/
-			if(num%2==0)
-				var=true;
-			else
-				var=false;
-		}// end of constructor
-
+	public P094_Vellore_Panel(int num) {
+		/* A parameter is sent to the constructor and if this number is even then it is set to true*/
+		if(num%2==0)
+			var=true;
+		else
+			var=false;
+		
+		
+		//checks if number passed is true or false and performs the following actions
+		if(var==false)
+			setBackground(NewColors.LIGHTBLUE.setColor());
+		else
+			setBackground(NewColors.WHITE.setColor());
+		
+		
+		//setting the size of the panel x,y axis
+		setLayout(new BorderLayout());
+        setSize(100,100);
+        
+			
+		//Resetting the font and alignment for both the variables
+		Font font = new Font("Papyrus", Font.PLAIN, 15);
+		name.setFont(font);
+		timer.setFont(font);		
+		name.setHorizontalAlignment(JLabel.CENTER);
+	    timer.setHorizontalAlignment(JLabel.CENTER);
+	    
+		
+	    //adding labels to the panel
+		add(BorderLayout.NORTH,name);
+		add(timer);
+		setVisible(true);
+		
+		
+		//method call for the timer
+		counter();
+		
+	}// end of constructor
 	
-	public void TimerIncrement() {
+	
+	//setting up counter.
+	public void counter() {
+		if(var==true) 
+			timerIncrement();
+		else
+			timerDecrement();
+	}
+	
+		
+	public void timerIncrement() {
+		/* Start the thread and run from 0-9 */
 		new Thread() {
 	        int counter = 0;
 	        public void run() {
@@ -94,8 +107,8 @@ public class P094_Vellore_Panel {
 	}//end or function. 
 	
 	
-	public void TimerDecrement() {
-		/* Start the thread and run from 0-9 */
+	public void timerDecrement() {
+		/* Start the thread and run from 9-0 */
 		new Thread() {
 	        int counter = 9;
 	        public void run() {
@@ -113,15 +126,13 @@ public class P094_Vellore_Panel {
 	    }.start();
 	}//end or function. 
 	
-	
 	public static void main(String[] args) {
 		
 		P094_Vellore_Panel object= new P094_Vellore_Panel(4);
-		JPanel a =object.P094_Vellore_Panel();
 		JFrame b=new JFrame(" ");
 		b.setVisible(true);
 		b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		b.getContentPane().add(a);
+		b.getContentPane().add(object, BorderLayout.CENTER);
 		b.pack();
 		
 	}// end of main
