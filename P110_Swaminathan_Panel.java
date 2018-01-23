2966191919a008d1b3ce79a732e4ae6bab571ea0
sangeetha151294 <sangeetha151294@gmail.com>
2018-01-23 14:16:20 -0700

    Code clean up.

--- P110_Swaminathan_Panel.java
+++ P110_Swaminathan_Panel.java
@@ -1,31 +1,25 @@
 
 import java.awt.*;
-
-import java.awt.GridLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
-* Lab 1
-* @author Sangeetha Swaminathan
-* P110_Swaminathan_Panel is a panel.
-* Row one is the first name.
-* Row two is the last name.
-* Row three is a counter that is either incrementing from 0 or decrementing from 9.
-* The mode of the counter and the background color are determined by the value passed to the constructor
-*/ 
+ * Lab 1
+ * @author Sangeetha Swaminathan
+ * @version 1.0
+ * P110_Swaminathan_Panel is a panel.
+ * Row one is the first name.
+ * Row two is the last name.
+ * Row three is a counter that is either incrementing from 0 or decrementing from 9.
+ * The mode of the counter and the background color are determined by the value passed to the constructor
+ */
 public class P110_Swaminathan_Panel extends JPanel {
-
+    
     
     GridBagConstraints gbc = new GridBagConstraints();
-    static int value =  0;
-    static int step = 1;
+    Font font = new Font("Papyrus", Font.PLAIN, 15);
+    Color blue = new Color (173,216,230);
     
     
     /**
@@ -36,51 +30,40 @@ public class P110_Swaminathan_Panel extends JPanel {
      */
     
     public P110_Swaminathan_Panel(int typeSwitch) {
-       
-        setLayout(new GridBagLayout());
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 1;
-        gbc.gridy = 1;
         
+        setLayout(new GridBagLayout());
+        setPreferredSize(new Dimension(100, 100));
         
         JLabel firstname = new JLabel("Sangeetha", JLabel.CENTER);
         JLabel lastname = new JLabel("Swaminathan", JLabel.CENTER);
-        firstname.setFont(new Font("Papyrus", Font.PLAIN, 15));
-        lastname.setFont(new Font("Papyrus", Font.PLAIN, 15));
+        firstname.setFont(font);
+        lastname.setFont(font);
         addAtPosition(firstname, 0, 0);
         addAtPosition(lastname, 0, 1);
-       
-       
-        if(typeSwitch % 2 == 0)
-        {
+        
+        if (typeSwitch % 2 == 0) {
             this.setBackground(Color.white);
+        } else {
+            this.setBackground(blue);
+            
         }
-        else
-        {
-            this.setBackground(new Color(173,216,230));
-           
-        }
-      
+        
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                int value=0;
+                int value = 0;
                 int step = 1;
                 
-                if(typeSwitch % 2 == 1)
-                {
-                     value = 9;
-                     step = -1;
+                if(typeSwitch % 2 == 1) {
+                    value = 9;
+                    step = -1;
                 }
                 
                 JLabel counter = new JLabel(new Integer(value).toString());
                 counter.setHorizontalAlignment(JLabel.CENTER);
-                counter.setFont(new Font("Papyrus", Font.PLAIN, 15));
+                counter.setFont(font);
                 addAtPosition(counter, 0, 2);
                 while (true) {
-                    
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -89,11 +72,10 @@ public class P110_Swaminathan_Panel extends JPanel {
                     }
                     
                     value = value + step;
-                    if(typeSwitch % 2 == 1 && value == -1)
-                    {
+                    if(typeSwitch % 2 == 1 && value < 0) {
                         value = 9;
                     }
-                    counter.setText(new Integer(Math.abs(value%10)).toString())    ;
+                    counter.setText(new Integer(Math.abs(value % 10)).toString());
                 }
             }
         });
@@ -101,10 +83,10 @@ public class P110_Swaminathan_Panel extends JPanel {
         setVisible(true);
     }
     
-    
     void addAtPosition(Component component, int x, int y) {
         gbc.gridx = x;
         gbc.gridy = y;
         add(component, gbc);
     }
 }
+
