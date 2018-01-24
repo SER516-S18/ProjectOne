944144c31709189622a541a293c3ae4d0de31c08
sharmarhythm <35712824+sharmarhythm@users.noreply.github.com>
2018-01-23 18:03:33 -0700

    Refactored variables and comments

--- P103_Sharma_Panel.java
+++ P103_Sharma_Panel.java
@@ -17,92 +17,93 @@ import javax.swing.*;
 
 public class P103_Sharma_Panel extends JPanel {
 
-	int counter;
-	Timer timer = new Timer();
-	boolean flag;
+	static int counter;
+	static boolean timerIncrements;
+	JLabel nameLabel;
+	JLabel timerLabel;
+	final Color LIGHTBLUE = new Color(173, 216, 230);
+	final int CENTER_ALIGNMENT = SwingConstants.CENTER;
+	final int BOTTOM_ALIGNMENT = SwingConstants.BOTTOM;
+	final int TOP_ALIGNMENT = SwingConstants.TOP;
+	final int FONT_STYLE = Font.PLAIN;
 
-	P103_Sharma_Panel(int num) {
+	/*
+	 *  Constructor sets the dimension for panel. It initializes nameLabel 
+	 *  and timerLabel. Constructor accepts an integer and set the counter
+	 *  value, panel background for even - odd integers separately.
+	 */
+	P103_Sharma_Panel(int timeColorValue) {
 
-		// Set the layouts for panel
 		setPreferredSize(new Dimension(100, 100));
 		setLayout(new GridLayout(2, 1, 0, 0));
-		// Label for displaying the name
-		JLabel nameLabel = new JLabel("<html>Rhythm<br>Sharma</html>", SwingConstants.CENTER);
-		nameLabel.setFont(new Font("Papyrus", Font.PLAIN, 15));
-		// Label for displaying the counter
-		JLabel timerLabel = new JLabel(String.valueOf(counter), SwingConstants.CENTER);
-		timerLabel.setFont(new Font("Papyrus", Font.PLAIN, 15));
-		// Set the text in center of the panel and counter after the name
-		nameLabel.setVerticalAlignment(SwingConstants.BOTTOM);
-		timerLabel.setVerticalAlignment(SwingConstants.TOP);
-		timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		nameLabel = new JLabel("<html>Rhythm<br>Sharma</html>", CENTER_ALIGNMENT);
+		nameLabel.setFont(new Font("Papyrus", FONT_STYLE, 15));
+		timerLabel = new JLabel(String.valueOf(counter), CENTER_ALIGNMENT);
+		timerLabel.setFont(new Font("Papyrus", FONT_STYLE, 15));
+		
+		// Aligning the texts in the panel 
+		nameLabel.setVerticalAlignment(BOTTOM_ALIGNMENT);
+		timerLabel.setVerticalAlignment(TOP_ALIGNMENT);
+		timerLabel.setHorizontalAlignment(CENTER_ALIGNMENT);
 
-		// Set the background and counter value
-		if (num % 2 == 0) {
+		if (timeColorValue % 2 == 0) {
 			setBackground(Color.WHITE);
-			flag = true;
+			timerIncrements = true;
 			counter = 0;
 		} else {
-			flag = false;
+			timerIncrements = false;
 			counter = 9;
-			setBackground(new Color(173, 216, 230)); // RGB for Light-blue color = 173,216,230
+			setBackground(LIGHTBLUE);
 		}
-
-		// Set the timer to run in an infinite loop
-		try {
-			TimerTask task = new TimerTask() {
-
-				@Override
-				public void run() {
-
-					// Counter value set to the label
-					timerLabel.setText("" + counter);
-
-					if (flag == true) {
-						counter++;
-						if (counter > 9) {
-							counter = 0;
-						}
-					} else {
-						counter--;
-						if (counter < 0) {
-							counter = 9;
-						}
-					}
-				}
-			};
-			// Counter will start immediately once the panel is
-			// called; Interval of the timer is 1 second.
-			timer.scheduleAtFixedRate(task, 0, 1000);
-		} catch (Exception e) {
-			e.printStackTrace(); // Print the runtime errors
-		}
-
-		// Add the labels to the panel
 		add(nameLabel);
 		add(timerLabel);
 
+		// Set the timer
+		scheduleTimer(timerLabel);
+					
 	}
 	
-	public static void main(String[] args) {
-		 
-		 JFrame jFrame = new JFrame(); JPanel jPanel = new JPanel(); jPanel.add(new
-		 P103_Sharma_Panel(101)); jFrame.add(jPanel); jFrame.setSize(1000, 500);
-		 jFrame.setVisible(true);
-		 jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/*
+	 * Set the timer counter and display using timerLabel.
+	 * Timer will start as soon as panel is called.
+	 * Interval of the timer is 1 second.
+	 */
+	
+	public static void scheduleTimer(JLabel timerLabel) {
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				timerLabel.setText("" + counter);
+				if (timerIncrements == true) {
+					counter++;
+					if (counter > 9) {
+						counter = 0;
+					}
+				} else {
+					counter--;
+					if (counter < 0) {
+						counter = 9;
+					}
+				}
+			}
+		};
+		try {
+			timer.scheduleAtFixedRate(task, 0, 1000);
+		}
+		catch (Exception e) {
+				e.printStackTrace();
+		}
 	}
 
-	/*
-	 * Main Method to execute the panel with frame
-	 */
+	public static void main(String[] args) {
 
-	/*
-	 * public static void main(String[] args) {
-	 * 
-	 * JFrame jFrame = new JFrame(); JPanel jPanel = new JPanel(); jPanel.add(new
-	 * P103_Sharma_Panel(101)); jFrame.add(jPanel); jFrame.setSize(1000, 500);
-	 * jFrame.setVisible(true);
-	 * jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); }
-	 */
-
+		JFrame jFrame = new JFrame();
+		JPanel jPanel = new JPanel();
+		jPanel.add(new P103_Sharma_Panel(101));
+		jFrame.add(jPanel);
+		jFrame.setSize(1000, 500);
+		jFrame.setVisible(true);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
