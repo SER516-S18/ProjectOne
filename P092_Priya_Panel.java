50a1e1b3c6e5a139b2cf38715af4cfd9b4824a05
aashita-priya <31880562+aashita-priya@users.noreply.github.com>
2018-01-23 22:49:42 -0700

    Update P092_Priya_Panel.java

--- P092_Priya_Panel.java
+++ P092_Priya_Panel.java
@@ -1,4 +1,4 @@
-
+import javax.swing.*;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -8,50 +8,43 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Logger;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 /**
  * P092_Priya_Panel.java
- * @author  Aashita Priya
+ *
+ * @author Aashita Priya
  */
 
-public class P092_Priya_Panel {
+public class P092_Priya_Panel extends JPanel {
 
     private int colorFlag;
+
+    //Timer for counter
+    Timer timer = new Timer();
+
+    private final int[] timeElapsedSec = {0};
+
+    //Creating labels with FirstName LastName and tick
+    private JLabel firstName = new JLabel("Aashita");
+    private JLabel lastName = new JLabel("Priya");
+    private JLabel tick = new JLabel();
 
     private static final Logger LOGGER = Logger.getLogger(P092_Priya_Panel.class.getSimpleName());
 
     /**
      * Constructor with a parameter for color flag
+     *
      * @param colorFlag
      */
     public P092_Priya_Panel(int colorFlag) {
         this.colorFlag = colorFlag;
-    }
 
-    /**
-     *
-     * @return JPanel Object to use
-     */
-    public JPanel getJPanel() {
+        this.setLayout(new GridBagLayout());
 
-        //Timer for counter
-        Timer timer = new Timer();
-
-        final int[] timeElapsedSec = {0};
-
-        //Creating a JPanel object
-        JPanel panel = new JPanel(new GridBagLayout());
         try {
 
             //Creating a Font object with given constraints
             Font font = new Font("Papyrus", Font.PLAIN, 15);
-
-            //Creating labels with FirstName LastName and tick
-            JLabel firstName = new JLabel("Aashita");
-            JLabel lastName = new JLabel("Priya");
-            JLabel tick = new JLabel();
 
             //Creating constraints for the positions of each label
             GridBagConstraints fNameConstraints = new GridBagConstraints();
@@ -59,7 +52,7 @@ public class P092_Priya_Panel {
             GridBagConstraints timerConstraints = new GridBagConstraints();
 
             //Setting preferred size of the panel according to the given specifications
-            panel.setPreferredSize(new Dimension(100, 100));
+            this.setPreferredSize(new Dimension(100, 100));
 
             //Setting constraints for FirstName Label
             fNameConstraints.gridx = 0;
@@ -79,14 +72,14 @@ public class P092_Priya_Panel {
             tick.setFont(font);
 
             //Add FirstName and LastName to the panel
-            panel.add(firstName, fNameConstraints);
-            panel.add(lastName, lNameConstraints);
+            this.add(firstName, fNameConstraints);
+            this.add(lastName, lNameConstraints);
 
             //The logic for Ticker
             //If the constructor is passing even then count up from 0 else count down from 9
             if (colorFlag % 2 == 0) {
                 timeElapsedSec[0] = 0;
-                panel.setBackground(Color.white);
+                this.setBackground(Color.white);
                 timer.schedule(new TimerTask() {
 
                     @Override
@@ -100,7 +93,7 @@ public class P092_Priya_Panel {
                 }, 1000, 1000);
             } else {
                 timeElapsedSec[0] = 9;
-                panel.setBackground(new Color(173,216,230));
+                this.setBackground(Color.cyan);
                 timer.schedule(new TimerTask() {
 
                     @Override
@@ -115,14 +108,12 @@ public class P092_Priya_Panel {
 
             }
             //Add Tick to the panel
-            panel.add(tick, timerConstraints);
+            this.add(tick, timerConstraints);
 
         } catch (Exception e) {
             LOGGER.severe("Exception Occurred:" + e.getMessage());
         }
-        return panel;
     }
-
     /**
      * How to use
      * 1. Create an object of P092_Priya_Panel [ P092_Priya_Panel panelCreate = new P092_Priya_Panel(1); ]
