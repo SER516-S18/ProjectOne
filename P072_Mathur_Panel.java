746b164b2d21983dab1c890873987dc35867464c
garv mathur <garv.mathur@gmail.com>
2018-01-23 17:23:04 -0700

    refactored code and added relevant comments

--- P072_Mathur_Panel.java
+++ P072_Mathur_Panel.java
@@ -3,12 +3,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 /**
- * P072_Mathur_Panel class creates a label with first and and last name displayed in 2 line
+ * P072_Mathur_Panel class creates a label with first and and last name displayed in 2 lines
  * The timer displays number from 0-9
- * The background is set depending on the value of the parameter sent to the constructor
+ * The background color is set depending on the value of the parameter sent to the constructor
  * @author  Garv Mathur
+ * @version 1.0
  */
 
 public class P072_Mathur_Panel extends JPanel {
@@ -19,8 +19,13 @@ public class P072_Mathur_Panel extends JPanel {
     public final int WIDTH = 100;
     public final int HEIGHT = 100;
     public final int DELAY = 1000;
+    public final Color LIGHT_BLUE = new Color(173,216,230);
 
-    // Constructor
+    /**
+     * @param num
+     * if value of num is odd background is set to light blue and counter moves from 9-0.
+     * if the value is even background is set to white and the counter moves from 0 to 9.
+     */
     public P072_Mathur_Panel(int num){
         try{
             this.num = num;
@@ -36,7 +41,11 @@ public class P072_Mathur_Panel extends JPanel {
 
     }
 
-    // Sets the Counter Value depending on the parameter being odd or even
+    /**
+     * This function sets the counter Value depending on the parameter being odd or even
+     * @param Nothing
+     * @return Nothing.
+     */
     private void setCounterValue() {
         if(isEven()){
             counterValue = 0;
@@ -46,13 +55,17 @@ public class P072_Mathur_Panel extends JPanel {
         }
     }
 
-    // Starts the clock timer. If the initial parameter is set as even the value increments. If odd it decrements.
+    /**
+     * Starts the clock timer
+     * If the initial parameter (num) is set as even the value increments.
+     * If odd it decrements.
+     * @param Nothing
+     * @return Nothing.
+     */
     private void setClockTimer() {
         clock = new JLabel(String.valueOf(counterValue));
         clock.setFont(new Font("Papyrus",Font.PLAIN,15));
         this.add(clock);
-
-        // Starts the timer
         ActionListener taskPerformer = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -74,39 +87,48 @@ public class P072_Mathur_Panel extends JPanel {
         new Timer(DELAY, taskPerformer).start();
     }
 
-    // Sets Background color of panel. If odd then sets light blue else sets White.
+    /**
+     * Sets Background color of panel. If odd then sets light blue else sets white
+     * @param Nothing
+     * @return Nothing.
+     */
     private void setBackgroundColor() {
 
         if(isEven()){
             this.setBackground(Color.WHITE);
         }
         else{
-            Color lightBlue = new Color(173,216,230);
-            this.setBackground(lightBlue);
+            this.setBackground(LIGHT_BLUE);
         }
     }
-    // Initialize and Add  Name Label to panel
+
+    /**
+     * Initialize and add  Name Label to panel
+     * @param Nothing
+     * @return Nothing.
+     */
     private void initializeAddNameLabel() {
         fullName = new JLabel("<html>Garv<br>Mathur</html>");
         fullName.setFont(new Font("Papyrus",Font.PLAIN,15));
         this.add(fullName);
     }
 
-    // Set the preferred panel size
+    /**
+     * Set the preferred panel size
+     * @param Nothing
+     * @return Nothing.
+     */
     private void setPanelSize() {
         this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
     }
 
-    // Checks if the initial configuration int value is even or odd
+    /**
+     * Checks if the initial num value passed in constructor is even or odd
+     * @param Nothing
+     * @return true/false.
+     */
     private boolean isEven(){
         return num % 2 == 0;
     }
 
-//    public static void main(String [] s) {
-//        JFrame jFrame = new JFrame();
-//        jFrame.setContentPane(new P072_Mathur_Panel(4));
-//        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        jFrame.pack();
-//        jFrame.setVisible(true);
-//    }
 }
