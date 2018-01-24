812d0394794dae027030fcbdf454984a9f0e8ebe
Varun <varun1309@bitbucket.org>
2018-01-23 18:24:47 -0700

    Added try catch

--- P107_Srivastava_Tab.java
+++ P107_Srivastava_Tab.java
@@ -30,32 +30,36 @@ public class P107_Srivastava_Tab extends JPanel{
 		gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         int argumentIndex = 104;
-		add(new P105_Siddiqui_Panel(++argumentIndex), gbc);
-		add(new P106_Singal_Panel(++argumentIndex), gbc);
-		add(new P108_SubramanianVenkatraman_Panel(++argumentIndex), gbc);
-		add(new P109_Suryawanshi_Panel(++argumentIndex), gbc);
-		add(new P110_Swaminathan_Panel(++argumentIndex), gbc);
-		add(new P111_Tandon_Panel(++argumentIndex), gbc);
-		add(new P112_Thakkar_Panel(++argumentIndex).ReturnObject(), gbc); //Had to call extra function
-		add(new P113_Thiagarajan_Panel(++argumentIndex), gbc);
-		add(new P114_Thoram_Panel(++argumentIndex), gbc);
-		add(new P115_Tikku_Panel(++argumentIndex), gbc);
-		add(new P116_Tran_Panel(++argumentIndex), gbc);
-		add(new P117_VasantraoYadav_Panel(++argumentIndex), gbc);
-		add(new P118_VelaudampalayamKesavan_Panel(++argumentIndex), gbc);
-		add(new P119_Vemula_Panel(++argumentIndex), gbc);
-		add(new P120_Venkatasubramanian_Panel(++argumentIndex), gbc);
-		add(new P121_Venugopal_Panel(++argumentIndex), gbc);
-		add(new P122_Vyas_Panel(++argumentIndex), gbc);
-		add(new P123_Wakchaure_Panel(++argumentIndex), gbc);
-		add(new P124_Wang_Panel(++argumentIndex), gbc);
-		add(new P125_Wathodkar_Panel(++argumentIndex), gbc);
-		add(new P126_Wei_Panel(++argumentIndex).getPanel(), gbc); //Had to call extra function
-		add(new P127_Wu_Panel(++argumentIndex).create(), gbc); //Had to call extra function
-		add(new P128_Yadamreddi_Panel(++argumentIndex).constructPanel(argumentIndex), gbc); //Had to call extra function
-		add(new P129_Yan_Panel(++argumentIndex), gbc);
-		add(new P130_Zhang_Panel(++argumentIndex), gbc);
-		// add(new P131_Zhou_Panel(131), gbc); //This guy is included in group 2 as P030 is the main window
+        try {
+			add(new P105_Siddiqui_Panel(++argumentIndex), gbc);
+			add(new P106_Singal_Panel(++argumentIndex), gbc);
+			add(new P108_SubramanianVenkatraman_Panel(++argumentIndex), gbc);
+			add(new P109_Suryawanshi_Panel(++argumentIndex), gbc);
+			add(new P110_Swaminathan_Panel(++argumentIndex), gbc);
+			add(new P111_Tandon_Panel(++argumentIndex), gbc);
+			add(new P112_Thakkar_Panel(++argumentIndex).ReturnObject(), gbc); //Had to call extra function
+			add(new P113_Thiagarajan_Panel(++argumentIndex), gbc);
+			add(new P114_Thoram_Panel(++argumentIndex), gbc);
+			add(new P115_Tikku_Panel(++argumentIndex), gbc);
+			add(new P116_Tran_Panel(++argumentIndex), gbc);
+			add(new P117_VasantraoYadav_Panel(++argumentIndex), gbc);
+			add(new P118_VelaudampalayamKesavan_Panel(++argumentIndex), gbc);
+			add(new P119_Vemula_Panel(++argumentIndex), gbc);
+			add(new P120_Venkatasubramanian_Panel(++argumentIndex), gbc);
+			add(new P121_Venugopal_Panel(++argumentIndex), gbc);
+			add(new P122_Vyas_Panel(++argumentIndex), gbc);
+			add(new P123_Wakchaure_Panel(++argumentIndex), gbc);
+			add(new P124_Wang_Panel(++argumentIndex), gbc);
+			add(new P125_Wathodkar_Panel(++argumentIndex), gbc);
+			add(new P126_Wei_Panel(++argumentIndex).getPanel(), gbc); //Had to call extra function
+			add(new P127_Wu_Panel(++argumentIndex).create(), gbc); //Had to call extra function
+			add(new P128_Yadamreddi_Panel(++argumentIndex).constructPanel(argumentIndex), gbc); //Had to call extra function
+			add(new P129_Yan_Panel(++argumentIndex), gbc);
+			add(new P130_Zhang_Panel(++argumentIndex), gbc);
+			// add(new P131_Zhou_Panel(++argumentIndex), gbc); //This guy is included in group 2 as P030 is the main window
+		} catch(Exception e){
+			System.out.println("Caught an exception while adding subpanels to P107_Srivastava_Tab tab: " + e.getMessage());
+		}
 	}
 
 }
