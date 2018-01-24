a33594e5144f9cd1fa63357033f7efe01d1826c0
madhavilathab9 <madhavilathab9@gmail.com>
2018-01-23 20:20:22 -0700

    cleaner code along with comments

--- P015_Bodeddula_Panel.java
+++ P015_Bodeddula_Panel.java
@@ -10,28 +10,40 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-/**
- * 
+/**The program displays a panel with my full name
+ * and it also reads an integer value and displays a timer
+ * if the integer value is even then timer increments with white background
+ * and if integer is odd the timer decrements with blue background  
  */
 
 /**
  * @author Madhavi Latha
  *
  */
+
+/*
+ * first name, last name, light blue are declared as variables 
+ * so that it is easy to change their values when ever required
+ */
 public class P015_Bodeddula_Panel extends JPanel{
     private int count ;
 	private static String firstName = "Madhavi Latha";
 	private static String lastName = "Bodeddula";
 	public static Color lightBlue = new Color(173, 216, 230);
-	  private JLabel timerLabel;
+	private JLabel timerLabel;
 
 
-
+/*
+ * initializing the timer value based on integer input
+ * and displaying the output on panel with proper font and alignment
+ */
 public P015_Bodeddula_Panel(int counter) {
-	if(counter%2 ==0) {
+	if(counter%2 ==0) 
+	{
 		count = 0;
 	}
-	else {
+	else 
+	{
 		count =9;
 	}
 	 setSize(100,100);
@@ -40,6 +52,7 @@ public P015_Bodeddula_Panel(int counter) {
      JLabel firstNameLabel = new JLabel(firstName,JLabel.CENTER);
      JLabel lastNameLabel = new JLabel(lastName, JLabel.CENTER); 
      timerLabel = new JLabel("", JLabel.CENTER);
+     
      firstNameLabel.setFont(new Font("Papyrus", Font.PLAIN, 15));
      lastNameLabel.setFont(new Font("papyrus", Font.PLAIN, 15));
      timerLabel.setFont(new Font("Papyrus", Font.PLAIN, 15));
@@ -50,25 +63,34 @@ public P015_Bodeddula_Panel(int counter) {
      startTimer(counter);
 }
 
-
+/*
+ * The startTimer method consists of the necessary code
+ * which increment or decrements the timer according to requirements
+ * and displays it to the panel.
+ * try and catch statements are also added to handle exceptions.
+ */
 
 private void startTimer(int counter) {
 	
 	try {
-        Timer timer = new Timer(1000, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	if(counter %2 == 0) {
+        Timer timer = new Timer(1000, new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+            	if(counter %2 == 0) 
+            	{
             		timerLabel.setText("" + count);
-            		if(count ==9) {
-            			count = 0;
+            		if(count ==9) 
+            		{
+            			count = -1;
             		} count++;
             	} 
             	else {
             		setBackground(lightBlue);
-            		//count = 9;
             		timerLabel.setText("" + count);
-            		if(count == 0) {
-            			count = 9;
+            		if(count == 0) 
+            		{
+            			count = 10;
             		} count--;
             		
             		}
@@ -82,5 +104,5 @@ private void startTimer(int counter) {
         System.out.print(e.getMessage());
     }
 }
-     
+             
 }
