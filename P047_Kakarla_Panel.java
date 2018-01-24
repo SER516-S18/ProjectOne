7be7f04e14d28941752aaa6b79e3be824b69775d
kvsshirisha <35435175+kvsshirisha@users.noreply.github.com>
2018-01-23 22:11:33 -0800

    Updated the changes needed.

--- P047_Kakarla_Panel.java
+++ P047_Kakarla_Panel.java
@@ -1,36 +1,39 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
+
+
 /**
- * This class implements the panel with 3 Labels- First Name, Last Name and a timer increasing and decreasing
- * and the background color of the label changing depending upon the input provided.
+ * This class implements the panel with 3 Labels- First Name, Last Name and a timer 
+ * increasing and decreasing and the background color of the label changing 
+ * depending upon the input value provided.
  * 
  * Completion Time: 3 hours
  * 
  * @author Shirisha Kakarla
- * @version 1/21/2018
+ * @version 1/23/2018
  */
 
-import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class P047_Kakarla_Panel extends JPanel {
 	
 	private JLabel firstname; 
 	private JLabel lastname;
-	
 	private JLabel count;
-	
 	private boolean even;
+	Color lightBlue = new Color(189,214,238);
 	
-	//constructor.
-	public P047_Kakarla_Panel(int n) {
-		/* params: integer 
-		 * return: None
-		 * Description: @para n is set to true if n is even otherwise to false.
-		 */
-		if(n%2==0)
+	/* params: integer 
+	 * return: None
+	 * Description: @para value is set to true if value is even otherwise to false.
+	 */
+	
+	public P047_Kakarla_Panel(int value) {
+		
+		if(value%2==0)
 		{
 			even=true;
 		}
@@ -46,7 +49,7 @@ public class P047_Kakarla_Panel extends JPanel {
 		}
 		else
 		{
-			this.setBackground(new Color(51,204,255));
+			this.setBackground(lightBlue);
 		}
 		
 		this.setPreferredSize(new Dimension(100,100));
@@ -55,9 +58,12 @@ public class P047_Kakarla_Panel extends JPanel {
 		//setting up JLabel for firstname and lastname in 2 different lines.
 		this.firstname=new JLabel(" Venkat Sai Shirisha");
 		this.lastname= new JLabel(" Kakarla");
+		this.firstname.setFont(new Font("Papyrus",Font.PLAIN,15));
+		this.lastname.setFont(new Font("Papyrus",Font.PLAIN,15));
 		
 		//setting up the JLabel for the Timer.
 		this.count=new JLabel();
+		this.count.setFont(new Font("Papyrus",Font.PLAIN,15));
 		
 		this.add(firstname);
 		this.add(lastname);
@@ -72,11 +78,13 @@ public class P047_Kakarla_Panel extends JPanel {
 			P047_Kakarla_CounterDecrement();
 	}
 	
+	/* params: None 
+	 * return: Void. 
+	 * Description: Thread starts and increases the counter from 0-9
+	 */
+	
 	public void P047_Kakarla_CounterIncrement() {
-		/* params: None 
-		 * return: Void. 
-		 * Description: Thread starts and increases from 0-9
-		 */
+		
 		new Thread() {
 	        int counter = 0;
 	        public void run() {
@@ -94,12 +102,13 @@ public class P047_Kakarla_Panel extends JPanel {
 	    }.start();
 	} 
 	
+	/* params: None 
+	 * return: Void 
+	 * Description: Thread starts and decreases the counter from 9-0
+	 */
 	
 	public void P047_Kakarla_CounterDecrement() {
-		/* params: None 
-		 * return: Void 
-		 * Description: Thread starts and decreases from 9-0
-		 */
+		
 		new Thread() {
 	        int counter = 9;
 	        public void run() {
@@ -116,4 +125,3 @@ public class P047_Kakarla_Panel extends JPanel {
 	    }.start();
 	} 
 }
-
