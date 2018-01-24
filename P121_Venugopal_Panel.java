a787b2dc1abce9a7bf291e7048794ecc6dad74da
pvenugo <32775579+pvenugo@users.noreply.github.com>
2018-01-23 17:59:25 -0700

    Update P121_Venugopal_Panel.java
    add evenNumFunction and oddNumFunction , Created color object for light blue

--- P121_Venugopal_Panel.java
+++ P121_Venugopal_Panel.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -9,102 +8,106 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
-*
-* @author Prasanth Venugopal (ASU: pvenugo1)
-* @version 1.0.0 (01/21/2018)
-* @purpose This class is a Jpanel consisting of Name (First Name, Last Name )along with counter and background value based on Parameter
-*/
+ *
+ * @author Prasanth Venugopal (ASU: pvenugo1)
+ * @version 1.0.0 (01/21/2018)
+ * @purpose This class is a Jpanel consisting of Name (First Name, Last Name )along with counter and background value based on Parameter
+ */
 
 class P121_Venugopal_Panel extends JPanel{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	JLabel count=new JLabel();
-	JPanel panel =new JPanel();
+ 
+    private static final long serialVersionUID = 1L;
+    private static final Color lightBlue =new Color(173,216,230);
     
-	public P121_Venugopal_Panel(int num) {
-		/* This is a constructor
-		 * @param num
-		 */
-		setAlignment();
-		if(num%2 == 0)
-		{
-			/*
-			 * If num is even , Background is set to white, counter is increased from 0 to 9 and place in a continues loop
-			 */
-			setBackground(Color.white);
-			new Thread() {
-		        int counter = 0;
-		        public void run() {
-		            while(true) {
-		            		if(counter==10)
-		            			counter=0;
-		            		
-		                count.setText(" " + (counter));
-		                counter++;
-		                try{
-		                    Thread.sleep(1000);
-		                } catch(Exception e) {}
-		            }
-		        }
-		    }.start();
-		}
-			
-		else
-		{
-			/*
-			 * If num is even , Background is set to Blue , counter is Decreased from 9 to 0 and place in a continues loop 
-			 */
-			setBackground(new Color(173,216,230));
-			new Thread() {
-		        int counter = 9;
-		        public void run() {
-		            while(true) {
-		            		if(counter==-1)
-		            			counter=9;
-		                count.setText(" " + (counter));
-		                counter--;
-		                try{
-		                    Thread.sleep(1000);
-		                } catch(Exception e) {}
-		            }
-		        }
-		    }.start();
-		}
-		
-		
-			
-	}
-
-	public void setAlignment() {
-		/*
-		 * Output: Alignement purposes , Sets Gridlayout 
-		 */
-		setLayout(new GridLayout(2,1));
-		setPreferredSize(new Dimension(100,100));
-		JLabel name=new JLabel("<html> Prasanth <br> Venugopal <br> </html>");
-		// 2 Lines in 1 Jlabel
-		name.setHorizontalAlignment(JLabel.CENTER);
-		count.setHorizontalAlignment(JLabel.CENTER);
-		count.setFont(new Font("Papyrus", Font.PLAIN, 15));
-                name.setFont(new Font("Papyrus", Font.PLAIN, 15));
-		add(name);
-	        add(count);
-	}
-		
-	public static void main(String[] args) {
-		
-		/*
-		 * Main Method to test the JPanel
-	
-		JFrame f=new JFrame("Simple Window");
-		f.setLayout(new GridLayout());
-		f.setLocationRelativeTo( null );
-		f.getContentPane().add(new P121_Venugopal_Panel(5));
-		f.pack();
-		f.setVisible(true);
-		*/
-		
-	}
+    JLabel count=new JLabel();
+    JPanel panel =new JPanel();
+    
+    public P121_Venugopal_Panel(int num) {
+        /* This is a constructor
+         * @param num
+         */
+        setAlignment();
+        if(num % 2 == 0)
+        {
+            /*
+             * If num is even , Background is set to white, 
+             * counter is increased from 0 to 9 and place in a continues loop
+             */
+        	evenNumFunction();
+        }
+        else
+        {
+        	/*
+        	 * If num is odd, This function is called to change Background to Blue 
+        	 * Counter is decreased from 9 to 0 and is in continues loop
+        	 */
+        	oddNumFunction();
+        }
+    }
+    private void evenNumFunction() {
+    	/*
+    * Background is set to white, 
+    * counter is increased from 0 to 9 and place in a continues loop
+    */
+    	setBackground(Color.white);
+        new Thread() {
+            int counter = 0;
+            public void run() {
+                while(true) {
+                    if(counter==10)
+                        counter=0;
+                    
+                    count.setText(" " + (counter));
+                    counter++;
+                    try{
+                        Thread.sleep(1000);
+                    } catch(Exception e) {
+                    	e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+    
+    private void oddNumFunction() {
+     /*
+    	 * This function is called to change Background to Blue 
+    	 * Counter is decreased from 9 to 0 and is in continues loop
+    	 */
+    	 setBackground(lightBlue);
+         new Thread() {
+             int counter = 9;
+             public void run() {
+                 while(true) {
+                     if(counter==-1)
+                         counter=9;
+                     count.setText(" " + (counter));
+                     counter--;
+                     try{
+                         Thread.sleep(1000);
+                     } catch(Exception e) {
+                    	 e.printStackTrace();
+                     }
+                 }
+             }
+         }.start();
+    	
+    }
+    
+    private void setAlignment() {
+        /*
+         * Sets Layout (GridLayout), sets Dimmension, Jlabel for firstname and Last name
+         * Aligns the counter to center 
+         * Adds font and size to the label
+         */
+        setLayout(new GridLayout(2,1));
+        setPreferredSize(new Dimension(100,100));
+        JLabel name=new JLabel("<html> Prasanth <br> Venugopal <br> </html>");
+        name.setHorizontalAlignment(JLabel.CENTER);
+        count.setHorizontalAlignment(JLabel.CENTER);
+        count.setFont(new Font("Papyrus", Font.PLAIN, 15));
+        name.setFont(new Font("Papyrus", Font.PLAIN, 15));
+        add(name);
+        add(count);
+    }
 }
