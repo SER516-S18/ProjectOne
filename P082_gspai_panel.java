f0a2b41353a592af3670e4e4f059a0241addca10
Gopika Pai <gopikaspai@gmail.com>
2018-01-23 22:48:54 -0700

    Update P082_gspai_panel.java

--- P082_gspai_panel.java
+++ P082_gspai_panel.java
@@ -1,107 +1,119 @@
-//package gopika;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+package gopika;
+import javax.swing.*;
+import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 /**
  * P082_gspai_panel is a panel containing two JLables 
- * In this program we are creating a panel with the name and a counter that keeps incrementing. 
+ * In this program we are creating a panel with the name and a counter that 
+ * keeps incrementing. 
  *  
  * @author Gopika Pai
  * gspai@asu.edu
  */
-
+/**
+ * This file creates the JPanel and the color is assigned to the 
+ * frame on the basis of even and odd numbers 
+ */
 public class P082_gspai_panel extends JPanel{
 
 
-    public static int cntr = 0; //initializing counter to 0
-
+    public static int cntr = 0;
+/**
+ * Private members of the class
+ */
     private boolean even;
     private Color type_color;
+  
+    /**
+     *Constructor for the class 
+     * 
+     */
 
     public P082_gspai_panel(int val) {
+        /**
+         * Checks if the value is even, if it is even it assigns a blue color
+         * else assigns white
+         */
         if(val%2==0)
         {
             even=true;
             cntr=-1;
-            background_color = new Color(white);  // setting with white color if the value is even
+            type_color=Color.white;  
 
         }
         else
         {
             even=false;
             cntr=10;
-            background_color=new Color(173, 216, 230);// if value is odd we set with blue color
-		
+            type_color= new Color(173,216,230); 
 
-	}
+        }
+        background_color(type_color);
 
     }
-
+/**
+ * Here we set the frame and background of the panel along with 
+ * the font and the size 
+ */
     public void background_color(Color col) {
-        
-        setSize(100,100);//here we set the size of panel to 100 and 100
-		setLayout(new GridBagLayout());//aligns to the center
-        GridBagConstraints center1 = new GridBagConstraints();
-        setBackground(col);//initially setting the background color of thw whole panel
-		JLabel Name = new JLabel("<html>Gopika<br>Pai</html>");//Create and initialise Name label with the name of the person
-		center1.gridx=0;
-        center1.gridy=0;
-        
-        Name.setFont(new Font("Papyrus", Font.PLAIN, 15));
-        add(Name, center1);
-		
 
-        JLabel  Counter = new JLabel(String.valueOf(cntr));//Create and Initialise Counter label with the counter value
-        Counter.setFont(new Font("Papyrus", Font.PLAIN, 15));
-		
-		center1.gridx=0;//Set the column address to O for the leftmost element
+      
+        setSize(100,100);
+        setBackground(col);
+        setLayout(new GridBagLayout());
+        GridBagConstraints center1 = new GridBagConstraints();
+        JLabel Name = new JLabel("<html>Gopika<br>Pai</html>");
+        Name.setFont(new Font("Papyrus", Font.PLAIN, 10));
+        center1.gridx=0;
+        center1.gridy=0;
+        add(Name,center1);
+
+        JLabel  Counter = new JLabel(String.valueOf(cntr));
+        Counter.setFont(new Font("Papyrus", Font.PLAIN, 10));
+        center1.gridx=0;
         center1.gridy=1;
         add(Counter, center1);
         
-        //Timer value is wrapped in between try and catch to handle exceptions during runtime
+        /**
+         * Timer value is wrapped in between try and catch to handle exceptions during runtime
+         */
+        
         try {
 
             TimerTask new_counter = new TimerTask()
             {
-                public void run()// Methods to change the value of the counter. 
+                public void run()
                 {
                     if(even)
                     {
-                        cntr = (cntr+1)%10;//Counter runs from 0 to 9 if ID is even
+                        cntr = (cntr+1)%10;
 
 
                     }
                     else
                     {
                         if(cntr>0)
-                            cntr = (cntr-1)%10;//Counter runs from 9 to 0 if ID is odd
+                            cntr = (cntr-1)%10;
                         else
                             cntr=9;
 
 
                     }
 
-                    Counter.setText(String.valueOf(cntr));//Sets the value of Counter label with the current value and updates continously
+                    Counter.setText(String.valueOf(cntr));
                 }
             };
 
             Timer t1 = new Timer();
-            long Delay = 5L;//A set delay with which the counter needs to be updated
-            long TimeLapse = 1000L;//The time gap between successive counter
+            long Delay = 5L;
+            long TimeLapse = 1000L;
             t1.scheduleAtFixedRate(new_counter, Delay, TimeLapse);
 
         } catch(Exception e)
         {
-            e.printStackTrace();//Displays errors
+            e.printStackTrace();
         }
 
 
@@ -110,11 +122,11 @@ public class P082_gspai_panel extends JPanel{
 
 
 
-    public static void main(String[] args) {
+ /**   public static void main(String[] args) {
 
         JFrame sampleJframe1 = new JFrame("Gopika's Frame");
         sampleJframe1.setSize(1000,1000);
-        P082_gspai_panel obj1 = new P082_gspai_panel (6);
+        P082_gspai_panel obj1 = new P082_gspai_panel (5);
         sampleJframe1.setLayout(new GridLayout(5,5));
         sampleJframe1.add(obj1 );
         sampleJframe1.setVisible(true);
@@ -122,3 +134,4 @@ public class P082_gspai_panel extends JPanel{
 
     }
 }
+*/
