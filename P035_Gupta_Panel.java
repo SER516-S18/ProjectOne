b0854f00501a95ca2f287eb8adc18ceed787dfa4
Mahima Gupta <mahimagupta1195@gmail.com>
2018-01-23 22:43:34 -0700

    Update P035_Gupta_Panel.java

--- P035_Gupta_Panel.java
+++ P035_Gupta_Panel.java
@@ -3,13 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package leetcode;
+
+import java.util.Scanner;
+
+
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,69 +24,77 @@ import javax.swing.JPanel;
  *
  * @author mahimagupta
  */
-public class P035_Gupta_Panel extends JPanel implements Runnable {
+ class P035_Gupta_Panel extends JPanel implements Runnable {
 	
 	JLabel labelName;
 	JLabel timeLabel;
 	Integer counter=0;
+        public static Color LIGHT_BLUE = new Color(173,216,230);
         public final int HEIGHT = 100;
         public final int WIDTH = 100;
         public final int wait = 1000;
         Integer value;
-        int determineCounter;
-	 GridBagConstraints c;
+     
+         
         
 	P035_Gupta_Panel(int val)  
         {  
-	this.value=val;
-      // creating labels
-        labelName = new JLabel("<html><br>Mahima<br>Gupta<br><br><br><br><br><br><br></html>"); //JPanel with full namelabel,first namelabel on first row and last namelabel on the next row.
-        timeLabel= new JLabel("");
-    
+	value=val;
         
-        //Resetting the font for both the variables
+        labelName = new JLabel("<html>Mahima<br>Gupta</html>");
         labelName.setFont(new Font("Papyrus", Font.BOLD, 15));
+        labelName.setHorizontalAlignment(JLabel.CENTER);
+        labelName.setVerticalAlignment(JLabel.CENTER);
+        this.add(labelName);
+    
+      
+        timeLabel = new JLabel("");
         timeLabel.setFont(new Font("Papyrus", Font.BOLD, 15)); 
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
+        timeLabel.setVerticalAlignment(JLabel.CENTER);
+        this.add(timeLabel);
         
-        this.setLayout(new GridBagLayout());
+       this.setSize(HEIGHT,WIDTH); 
+       this.setLayout(new GridLayout(2, 0));
 
-        c = new GridBagConstraints();
-        c.gridx=0;                                                   
-	c.gridy=2;
-        //Giving attributes to  panel
-        this.setSize(HEIGHT,WIDTH); 
+       
+        
+      
        
          if(value%2==0) {
 
 			this.setBackground(Color.WHITE);
                         counter=0;
-                     }
+                        }
 		
-        else         {
-			this.setBackground(new Color(189,214,238));
+        else            {
+			this.setBackground(LIGHT_BLUE);
                         counter=9;
-		}
+                        }
 
 
-        this.add(labelName,c);
-        this.add(timeLabel,c);
+        
+     
 		
-      //Counter for displaying 0 to 9
+      
         Thread t = new Thread(this);
         t.start();
            
         } 
-	
 		@Override
 		public void run() {
 			
 			while(true){
-	            try{
+	            
                         if( value%2==0)
                         { 
                             if(counter <10)
                             {
-                            Thread.sleep(wait);
+                                try {
+                                    Thread.sleep(wait);
+                                } catch (InterruptedException ex) {
+                                    Logger.getLogger(P035_Gupta_Panel.class.getName()).log(Level.SEVERE, null, ex);
+                                }
                             timeLabel.setText(counter.toString()); //JLabel displaying numbers from 0-9
                             counter++;
                             }
@@ -89,39 +104,31 @@ public class P035_Gupta_Panel extends JPanel implements Runnable {
                         {
                         if(counter>=0)
                         {
-                        Thread.sleep(wait);
-                        timeLabel.setText(counter.toString()); //JLabel displaying numbers from 0-9
+                            try{
+                                Thread.sleep(wait);
+                                }
+                            catch (InterruptedException ex)
+                                {
+                                Logger.getLogger(P035_Gupta_Panel.class.getName()).log(Level.SEVERE, null, ex);
+                                 System.out.println("Error! Stopping the timer...");
+                                }
+                        timeLabel.setText(counter.toString()); //JLabel displaying numbers from 9-0
                         counter--;
+                        
+                               
                         }
                         else 
                             counter=9;
-                        }
                         
-	            }       catch (InterruptedException ex) {
-                                Logger.getLogger(P035_Gupta_Panel.class.getName()).log(Level.SEVERE, null, ex);
-                                 System.out.println("Error! Stopping the timer...");
-		    }   
-	            }
-	            }
+                        
+	                 
+                        }   
+                                }
+                                }
             
-	        }
-
-
-/*
-      public static void main(String args[])  
-        {  
-        JFrame frame=new JFrame("");
-     frame.add(panel);
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,400);    
-        frame.setLayout(null);    
-        frame.setVisible(true);
+	 }       	 
+                   
         
-      frame.add(new P035_Gupta_Panel(2));
-       
-        	 
-        	
-        }
 
-*/
+
 
