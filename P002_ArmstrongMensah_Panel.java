ffb87efa24afad75eb24a816672b22a9c3008180
Cephas Armstrong-Mensah <carmstr7@asu.edu>
2018-01-23 20:45:51 -0700

    added final comments and changes

--- P002_ArmstrongMensah_Panel.java
+++ P002_ArmstrongMensah_Panel.java
@@ -7,10 +7,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EtchedBorder;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.border.EmptyBorder;
 
 /**
- * This class is my Panel implementation using a parameterized constructor along
- * with private method init and class Counter
+ * This class is my Panel implementation using a parameterized constructor 
+ * which receives an integer and determines Panel Color and Counter Direction
+ * based on the parameter
  *
  * @author Cephas Armstrong-Mensah
  * @version 1.0
@@ -18,87 +26,79 @@ import javax.swing.border.EtchedBorder;
  *
  */
 public class P002_ArmstrongMensah_Panel extends JPanel {
-	/**
-	 * 
-	 * @param flag Passed along to private method
-	 */
-	public P002_ArmstrongMensah_Panel(int flag) {
-		this.init(flag);
-	}
+  // Declaring Color Constants
+  final Color LIGHTBLUE = new Color(171, 216, 230);
+  final Color WHITE = Color.WHITE;
 
-	/**
-	 * 
-	 * @param flag Used to determine the counter direction and panel color
-	 */
-	private void init(int flag) {
-		setBorder(new EtchedBorder(EtchedBorder.LOWERED, Color.BLACK, null));
-		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 15));
+  /**
+   * 
+   * @param flag Passed along to private method
+   */
+  public P002_ArmstrongMensah_Panel (int flag) {
+    setLayout(new GridLayout(2, 1, 0, 0));
+    
+    JLabel lblFullName = new JLabel("<html>Cephas<br>Armstrong-Mensah</html>");
+    lblFullName.setHorizontalAlignment(SwingConstants.CENTER);
+    lblFullName.setHorizontalTextPosition(SwingConstants.CENTER);
+    lblFullName.setFont(new Font("Papyrus", lblFullName.getFont().getStyle() &
+        ~Font.BOLD & ~Font.ITALIC, lblFullName.getFont().getSize() + 2));
+    add(lblFullName);
 
-		JLabel lblFullName = new JLabel("<html>Cephas<br>Armstrong-Mensah</html>");
-		lblFullName.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblFullName.setFont(new Font("Papyrus", Font.PLAIN, 10));
-		add(lblFullName);
+    JLabel lblCounter = new JLabel();
+    lblCounter.setHorizontalTextPosition(SwingConstants.CENTER);
+    lblCounter.setHorizontalAlignment(SwingConstants.CENTER);
+    lblCounter.setFont(new Font("Papyrus", lblCounter.getFont().getStyle() &
+        ~Font.BOLD & ~Font.ITALIC, lblCounter.getFont().getSize() + 6));
+    add(lblCounter);
+    
+    Counter counter = new Counter(lblCounter, (flag % 2 == 0) ? 0 : 9);
 
-		JLabel lblCounter = new JLabel("");
-		lblCounter.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblCounter.setFont(new Font("Papyrus", Font.PLAIN, 15));
-		lblCounter.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblCounter);
+    this.setBackground(flag % 2 == 0 ? WHITE : LIGHTBLUE);
+    this.setVisible(true);
+    new Thread(counter).start();
+  }
 
-		Counter counter = new Counter(lblCounter, (flag % 2 == 0) ? 0 : 9);
-		new Thread(counter).start();
+  /**
+   * This Counter Class implements Runnable which is a Thread idea option. The
+   * thread is used to run counter so it does not block the main thread
+   *
+   * @author Cephas Armstrong-Mensah
+   *
+   */
+  private class Counter implements Runnable {
+    private JLabel lblCounter;
+    private int counter;
+    private int direction;
 
-		this.setBackground(flag % 2 == 0 ? Color.WHITE : new Color(135, 206, 250));
-		this.setSize(100, 100);
-		this.setVisible(true);
-	}
+    /**
+     * Counter constructor to spin off a new thread
+     * 
+     * @param lblCounter JLabel used for displaying counter
+     * @param counter Initial counter value either 0 or 9
+     */
+    public Counter(JLabel lblCounter, int counter) {
+      this.lblCounter = lblCounter;
+      this.counter = counter;
+      this.direction = counter;
+    }
 
-	/**
-	 * This Counter Class implements Runnable which is a Thread idea option. The
-	 * thread is used to run counter so it does not block the main thread
-	 *
-	 * @author Cephas Armstrong-Mensah
-	 *
-	 */
-	private class Counter implements Runnable {
-		private JLabel lblCounter;
-		private int counter;
-		private int flag;
-
-		/**
-		 * Counter constructor to spin off a new thread
-		 * 
-		 * @param lbl JLabel used for displaying counter
-		 * @param c Initial counter value either 0 or 9
-		 */
-		public Counter(JLabel lbl, int c) {
-			this.lblCounter = lbl;
-			this.counter = c;
-			this.flag = c;
-		}
-
-		@Override
-		public void run() {
-			try {
-				while (true) {
-					try {
-						lblCounter.setText(this.counter + "");
-						if (flag == 0) {
-							if (++this.counter > 9)
-								this.counter = 0;
-						} else {
-							if (--this.counter < 0)
-								this.counter = 9;
-						}
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void run() {
+      while (true) {
+        lblCounter.setText(this.counter + "");
+        if (direction == 0) {
+          if (++this.counter > 9)
+            this.counter = 0;
+        } else {
+           if (--this.counter < 0)
+            this.counter = 9;
+        }
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
 }
